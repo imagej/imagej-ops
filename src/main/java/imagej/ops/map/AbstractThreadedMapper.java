396b641dd39f0c364b7df18681437f04478856b1
Martin Horn <martin.horn@uni-konstanz.de>
2014-03-07 20:52:18 +0100

    fix minor bugs in threaded Mapper

--- src/main/java/imagej/ops/map/AbstractThreadedMapper.java
+++ src/main/java/imagej/ops/map/AbstractThreadedMapper.java
@@ -18,70 +18,70 @@ import org.scijava.thread.ThreadService;
  */
 public abstract class AbstractThreadedMapper implements Op, Cancelable {
 
-	@Parameter
-	private ThreadService threadService;
+    @Parameter
+    private ThreadService threadService;
 
-	@Parameter
-	private String cancelationMessage;
+    private String cancelationMessage;
 
-	protected abstract void runThread(final int firstElement,
-			final int lastElement);
+    protected abstract void runThread(final int firstElement,
+            final int lastElement);
 
-	protected void runThreading(final long l) {
+    protected void runThreading(final long l) {
 
-		// TODO: is there a better way to determine the optimal chunk size?
-		final int numChunks = (int) (l / Runtime.getRuntime()
-				.availableProcessors());
+        // TODO: is there a better way to determine the optimal chunk size?
+        final int numChunks =
+                (int)(l / Runtime.getRuntime().availableProcessors());
 
-		final int chunkSize = (int) (l / numChunks);
+        final int chunkSize = (int)(l / numChunks);
 
-		final ArrayList<Future<?>> futures = new ArrayList<Future<?>>(numChunks);
+        final ArrayList<Future<?>> futures =
+                new ArrayList<Future<?>>(numChunks);
 
-		for (int i = 0; i < numChunks - 1; i++) {
-			futures.add(threadService.run(new ChunkedUnaryFunctionTask(i
-					* chunkSize, (i * chunkSize + chunkSize) - 1)));
-		}
+        for (int i = 0; i < numChunks - 1; i++) {
+            futures.add(threadService.run(new ChunkedUnaryFunctionTask(i
+                    * chunkSize, (i * chunkSize + chunkSize) - 1)));
+        }
 
-		// last chunk gets the rest
-		futures.add(threadService.run(new ChunkedUnaryFunctionTask(
-				(numChunks - 1) * chunkSize,
-				(int) (chunkSize + (l % chunkSize)))));
+        // last chunk gets the rest
+        futures.add(threadService
+                .run(new ChunkedUnaryFunctionTask((numChunks - 1) * chunkSize,
+                        (int)(chunkSize + (l % chunkSize)))));
 
-		for (final Future<?> future : futures) {
-			try {
-				future.get();
-			} catch (final Exception e) {
-				cancelationMessage = e.getMessage();
-				break;
-			}
-		}
-	}
+        for (final Future<?> future : futures) {
+            try {
+                future.get();
+            } catch (final Exception e) {
+                cancelationMessage = e.getMessage();
+                break;
+            }
+        }
+    }
 
-	@Override
-	public String getCancelReason() {
-		return cancelationMessage;
-	}
+    @Override
+    public String getCancelReason() {
+        return cancelationMessage;
+    }
 
-	@Override
-	public boolean isCanceled() {
-		return cancelationMessage != null;
-	}
+    @Override
+    public boolean isCanceled() {
+        return cancelationMessage != null;
+    }
 
-	private class ChunkedUnaryFunctionTask implements Runnable {
+    private class ChunkedUnaryFunctionTask implements Runnable {
 
-		private final int firstElement;
+        private final int firstElement;
 
-		private final int lastElement;
+        private final int lastElement;
 
-		public ChunkedUnaryFunctionTask(final int firstElement,
-				final int lastElement) {
-			this.firstElement = firstElement;
-			this.lastElement = lastElement;
-		}
+        public ChunkedUnaryFunctionTask(final int firstElement,
+                final int lastElement) {
+            this.firstElement = firstElement;
+            this.lastElement = lastElement;
+        }
 
-		@Override
-		public void run() {
-			runThread(firstElement, lastElement);
-		}
-	}
+        @Override
+        public void run() {
+            runThread(firstElement, lastElement);
+        }
+    }
 }
--- src/main/java/imagej/ops/map/ThreadedMapperII.java
+++ src/main/java/imagej/ops/map/ThreadedMapperII.java
@@ -18,43 +18,43 @@ import org.scijava.plugin.Plugin;
  * @param <A>
  * @param <B>
  */
-@Plugin(type = Op.class, name = "map", priority = Priority.LOW_PRIORITY + 1)
+@Plugin(type = Op.class, name = "map", priority = Priority.LOW_PRIORITY + 2)
 public class ThreadedMapperII<A, B> extends AbstractThreadedMapper implements
-		Contingent {
+        Contingent {
 
-	@Parameter
-	private IterableInterval<A> in;
+    @Parameter
+    private IterableInterval<A> in;
 
-	@Parameter
-	private UnaryFunction<A, B> func;
+    @Parameter
+    private UnaryFunction<A, B> func;
 
-	@Parameter(type = ItemIO.BOTH)
-	private IterableInterval<B> out;
+    @Parameter(type = ItemIO.BOTH)
+    private IterableInterval<B> out;
 
-	@Override
-	public void run() {
-		runThreading(in.size());
-	}
+    @Override
+    public void run() {
+        runThreading(in.size());
+    }
 
-	@Override
-	public boolean conforms() {
-		return in.iterationOrder().equals(out.iterationOrder());
-	}
+    @Override
+    public boolean conforms() {
+        return in.iterationOrder().equals(out.iterationOrder());
+    }
 
-	@Override
-	protected void runThread(final int firstElement, final int lastElement) {
-		final Cursor<A> inCursor = in.cursor();
-		inCursor.jumpFwd(firstElement - 1);
+    @Override
+    protected void runThread(final int firstElement, final int lastElement) {
+        final Cursor<A> inCursor = in.cursor();
+        inCursor.jumpFwd(firstElement - 1);
 
-		final Cursor<B> outCursor = out.cursor();
-		final UnaryFunction<A, B> copy = func.copy();
+        final Cursor<B> outCursor = out.cursor();
+        final UnaryFunction<A, B> copy = func.copy();
 
-		int ctr = 0;
-		while (inCursor.hasNext() && ctr < lastElement + 1) {
-			inCursor.fwd();
-			outCursor.fwd();
-			copy.compute(inCursor.get(), outCursor.get());
-			ctr++;
-		}
-	}
+        int ctr = 0;
+        while (inCursor.hasNext() && ctr < lastElement + 1) {
+            inCursor.fwd();
+            outCursor.fwd();
+            copy.compute(inCursor.get(), outCursor.get());
+            ctr++;
+        }
+    }
 }
