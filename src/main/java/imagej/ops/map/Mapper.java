cde4fa36df71fcd2535a14bb951f40095a96a19b
dietzc <christian.dietz@uni-konstanz.de>
2014-03-09 17:46:52 +0100

    Added MapperInterface

--- src/main/java/imagej/ops/map/Mapper.java
+++ src/main/java/imagej/ops/map/Mapper.java
@@ -1,73 +1,23 @@
-/*
- * #%L
- * ImageJ OPS: a framework for reusable algorithms.
- * %%
- * Copyright (C) 2014 Board of Regents of the University of
- * Wisconsin-Madison and University of Konstanz.
- * %%
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * #L%
- */
 
 package imagej.ops.map;
 
-import imagej.ops.AbstractFunction;
 import imagej.ops.Function;
 import imagej.ops.Op;
-import net.imglib2.Cursor;
-import net.imglib2.IterableInterval;
-import net.imglib2.RandomAccess;
-import net.imglib2.RandomAccessibleInterval;
-
-import org.scijava.Priority;
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
 
 /**
- * TODO
+ * Simple marker interface. A
  * 
- * @author Martin Horn
  * @author Christian Dietz
  */
-@Plugin(type = Op.class, name = "map", priority = Priority.LOW_PRIORITY)
-public class Mapper<A, B> extends
-	AbstractFunction<IterableInterval<A>, RandomAccessibleInterval<B>>
-{
-	@Parameter
-	private Function<A, B> func;
+public interface Mapper<A, B> extends Op {
 
-	@Override
-	public RandomAccessibleInterval<B> compute(final IterableInterval<A> input,
-		final RandomAccessibleInterval<B> output)
-	{
-		final Cursor<A> cursor = input.localizingCursor();
-		final RandomAccess<B> rndAccess = output.randomAccess();
+	/**
+	 * @return the {@link Function} to be mapped
+	 */
+	Function<A, B> getFunction();
 
-		while (cursor.hasNext()) {
-			cursor.fwd();
-			rndAccess.setPosition(cursor);
-			func.compute(cursor.get(), rndAccess.get());
-		}
-
-		return output;
-	}
+	/**
+	 * Set the {@link Function} to be mapped
+	 */
+	void setFunction(Function<A, B> func);
 }
