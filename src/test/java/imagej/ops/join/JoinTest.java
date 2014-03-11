da48c0a71877a957dbcafcf890e850224ad60809
dietzc <christian.dietz@uni-konstanz.de>
2014-03-11 16:12:49 +0100

    JoinTests: Finish join test

--- src/test/java/imagej/ops/join/JoinTest.java
+++ src/test/java/imagej/ops/join/JoinTest.java
@@ -16,15 +16,18 @@ import org.junit.Test;
 public class JoinTest extends AbstractOpTest {
 
 	private Img<ByteType> in;
+	private Img<ByteType> out;
 	private Op inplaceOp;
+	private Op functionalOp;
 
 	@Before
 	public void init() {
 		final long[] dims = new long[] { 10, 10 };
 		in = generateByteTestImg(false, dims);
-		inplaceOp = ops.op(Map.class, in, new AddOneInplace());
-//		buffer = generateByteTestImg(false, dims);
-//		out = generateByteTestImg(false, dims);
+		out = generateByteTestImg(false, dims);
+		inplaceOp = ops.op(Map.class, Img.class, new AddOneInplace());
+		functionalOp =
+			ops.op(Map.class, Img.class, Img.class, new AddOneFunctional());
 	}
 
 	@Test
@@ -34,6 +37,34 @@ public class JoinTest extends AbstractOpTest {
 
 		// test
 		final Cursor<ByteType> c = in.cursor();
+
+		while (c.hasNext()) {
+			org.junit.Assert.assertEquals(2, c.next().get());
+		}
+	}
+
+	@Test
+	public void testFunctionInplaceJoin() {
+		final Op op =
+			ops.op(FunctionInplaceJoin.class, out, in, functionalOp, inplaceOp);
+		op.run();
+
+		// test
+		final Cursor<ByteType> c = out.cursor();
+
+		while (c.hasNext()) {
+			org.junit.Assert.assertEquals(2, c.next().get());
+		}
+	}
+
+	@Test
+	public void testInplaceFunctionJoin() {
+		final Op op =
+			ops.op(InplaceFunctionJoin.class, out, in, inplaceOp, functionalOp);
+		op.run();
+
+		// test
+		final Cursor<ByteType> c = out.cursor();
 
 		while (c.hasNext()) {
 			org.junit.Assert.assertEquals(2, c.next().get());
