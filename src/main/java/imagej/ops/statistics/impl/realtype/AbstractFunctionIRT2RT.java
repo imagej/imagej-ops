6eddaa9b2617f0863031ce40dcf7059d32bb5fbd
dietzc <christian.dietz@uni-konstanz.de>
2014-03-12 13:04:34 +0100

    Added AbstractFunctionRT2RT

---
+++ src/main/java/imagej/ops/statistics/impl/realtype/AbstractFunctionIRT2RT.java
@@ -0,0 +1,14 @@
+
+package imagej.ops.statistics.impl.realtype;
+
+import imagej.ops.AbstractFunction;
+import net.imglib2.type.numeric.RealType;
+
+/**
+ * @author Christian Dietz
+ */
+public abstract class AbstractFunctionIRT2RT extends
+	AbstractFunction<Iterable<? extends RealType<?>>, RealType<?>>
+{
+	// NB: Marker to reduce amount of generics
+}
