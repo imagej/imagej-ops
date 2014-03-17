c4d822f43f8e7876ff354eb7ebea6e74c0171338
dietzc <christian.dietz@uni-konstanz.de>
2014-03-17 16:14:58 +0100

    Min/Max: Interfaces and implementations

---
+++ src/main/java/imagej/ops/descriptors/statistics/rt/MinIRT.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * ImageJ OPS: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package imagej.ops.descriptors.statistics.rt;
+
+import imagej.ops.Op;
+import imagej.ops.descriptors.statistics.Min;
+
+import java.util.Iterator;
+
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.DoubleType;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Calculate {@link Min} of {@link Iterable} of {@link RealType}.
+ * 
+ * @author Christian Dietz
+ */
+@Plugin(type = Op.class, name = Min.NAME, label = Min.LABEL,
+	priority = Priority.LOW_PRIORITY)
+public class MinIRT extends AbstractFunctionIRT implements
+	Min<Iterable<RealType<?>>, RealType<?>>
+{
+
+	@Override
+	public RealType<?> compute(final Iterable<RealType<?>> input,
+		RealType<?> output)
+	{
+		if (output == null) output = new DoubleType();
+
+		final Iterator<? extends RealType<?>> it = input.iterator();
+		RealType<?> min = it.next();
+
+		while (it.hasNext()) {
+			final RealType<?> next = it.next();
+			if (min.getRealDouble() > next.getRealDouble()) {
+				min = next;
+			}
+		}
+
+		output.setReal(min.getRealDouble());
+		return output;
+	}
+
+}
