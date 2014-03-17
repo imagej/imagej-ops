cccf671ae3b86dfe9f8dd2ed835d71cd4f5339db
dietzc <christian.dietz@uni-konstanz.de>
2014-03-17 16:24:55 +0100

    ProjectCommand: Using correct Mean

--- src/main/java/imagej/ops/commands/project/ProjectCommand.java
+++ src/main/java/imagej/ops/commands/project/ProjectCommand.java
@@ -33,8 +33,8 @@ package imagej.ops.commands.project;
 import imagej.command.Command;
 import imagej.ops.AbstractFunction;
 import imagej.ops.OpService;
+import imagej.ops.descriptors.statistics.Mean;
 import imagej.ops.project.Project;
-import imagej.ops.statistics.Mean;
 import net.imglib2.img.Img;
 import net.imglib2.meta.ImgPlus;
 import net.imglib2.meta.TypedAxis;
@@ -67,10 +67,10 @@ public class ProjectCommand<T extends RealType<T>> implements Command {
 	@Override
 	public void run() {
 		if (out == null) {
-			Img<T> img = in.factory().create(in, in.firstElement().createVariable());
+			final Img<T> img = in.factory().create(in, in.firstElement().createVariable());
 			out = new ImgPlus<T>(img, in);
 		}
-		int axisIndex = in.dimensionIndex(axis.type());
+		final int axisIndex = in.dimensionIndex(axis.type());
 		ops.run(Project.class, out, in, method, axisIndex);
 	}
 
@@ -83,7 +83,7 @@ public class ProjectCommand<T extends RealType<T>> implements Command {
 		private Mean<Iterable<T>, T> mean;
 
 		@Override
-		public T compute(Iterable<T> input, T output) {
+		public T compute(final Iterable<T> input, final T output) {
 			if (mean == null) {
 				mean = (Mean<Iterable<T>, T>) ops.op(Mean.class, output, input);
 			}
