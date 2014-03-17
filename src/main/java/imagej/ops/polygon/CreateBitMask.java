5774f4471bc744de1e19444b720cc8bf9cd40a57
dietzc <christian.dietz@uni-konstanz.de>
2014-03-17 16:18:05 +0100

    Added CreateBitmask and ContourExtraction

---
+++ src/main/java/imagej/ops/polygon/CreateBitMask.java
@@ -0,0 +1,10 @@
+
+package imagej.ops.polygon;
+
+import imagej.ops.Function;
+
+public interface CreateBitMask<I, O> extends Function<I, O> {
+
+	final static String NAME = "bitmask";
+	final static String LABEL = "Create BitMask";
+}
