d4c8af7e8e7ecabdfab2d99213725c5baa31acaa
Curtis Rueden <ctrueden@wisc.edu>
2014-04-03 10:43:14 -0500

    Rename JoinInplace to JoinInplaces
    Not totally happy with that name, but it is more consistent with
    "JoinFunctions" (both are explicitly plural now).
    
    This rename also makes room for a new interface called "JoinInplace" to
    match the "JoinFunction" interface.

--- src/main/java/imagej/ops/join/JoinInplaces.java
+++ src/main/java/imagej/ops/join/JoinInplaces.java
@@ -45,7 +45,7 @@ import org.scijava.plugin.Plugin;
  * @author Christian Dietz
  */
 @Plugin(type = Op.class, name = "join")
-public class JoinInplace<A> extends AbstractFunction<A, A> {
+public class JoinInplaces<A> extends AbstractFunction<A, A> {
 
 	// list of functions to be joined
 	private List<InplaceFunction<A>> functions;
