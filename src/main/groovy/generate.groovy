7cea6ecacc9b9946c4e0b1ff07f9a56213250128
Jonathan <squareys@googlemail.com>
2014-04-08 10:30:56 +0200

    Added utility functions to groovy script
    Added support for mutliline replacements
    Added support for replacements containing '='

--- src/main/groovy/generate.groovy
+++ src/main/groovy/generate.groovy
@@ -2,10 +2,51 @@ templateDirectory = project.properties['templateDirectory']
 outputDirectory = project.properties['outputDirectory']
 
 /*
+ * Unescapes a string
+ * "\\n" => '\n'
+ * "\\\\" => '\\'
+ */
+def unescape(string) {	
+	output = "";
+	
+	escaped = false;
+	for (int i = 0; i < string.length(); ++i) {
+		c = string.getAt( i );
+		if (c == '\\' && !escaped) {
+			escaped = true;
+		} else {
+			if (escaped && c == 'n') {
+				c = '\n';
+			}
+			escaped = false;
+			output += c;
+		}
+	}
+	
+	return output;
+}
+
+/*
+ * Finds a character c in str and returns the index this character was
+ * first found in str.
+ */
+def firstIndexOf(str, c) {
+	int index = -1;
+	for (int i = 0; i < str.length(); ++i) {
+		if (str.getAt(i) == c) {
+			index = i;
+			break;
+		}
+	}
+	
+	return index;
+}
+
+/*
  * Translates a template into many files in the outputDirectory,
  * given translations where each line has the form:
  *
- * [filename]~[pattern]=[replacement~[pattern2]=[replacement2]...
+ * [filename]~[pattern]=[replacement]~[pattern2]=[replacement2]...
  */
 def translate(template, translations) {
   /* read template */
@@ -27,11 +68,12 @@ def translate(template, translations) {
     translated = untranslated;
     pairs = line.split('~');
     for (i = 1; i < pairs.length; i++) {
-      split = pairs[i].split('=');
-      if (split.length != 2) {
+      split = firstIndexOf(pairs[i], '=');
+      if (split == -1) {
         throw new IllegalArgumentException("Illegal pair: '" + pairs[i] + "' in line '" + line + "'");
       }
-      translated = translated.replaceAll(split[0], split[1]);
+	  curPair = pairs[i]; //for convenience
+      translated = translated.replaceAll(curPair.substring(0, split), unescape(curPair.substring(split+1, curPair.length())));
     }
 
     // write out the file
@@ -46,4 +88,9 @@ def translate(template, translations) {
   reader.close();
 }
 
+/*
+ * Generate classes from templates and lists
+ */
 translate('Arithmetic.template', 'Arithmetic.list');
+translate('RealUnary.template', 'RealUnary_implementations.list');
+translate('RealUnaryInterface.template', 'RealUnary_interfaces.list');
