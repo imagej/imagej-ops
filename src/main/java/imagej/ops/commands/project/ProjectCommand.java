ad7b594473f72e2b174c104eec63068c7485e075
Daniel <daniel.seebacher@googlemail.com>
2014-05-23 14:35:55 +0200

    Use MeanIRT
    As it was done in imagej.ops.threshold.LocalMean

--- src/main/java/imagej/ops/commands/project/ProjectCommand.java
+++ src/main/java/imagej/ops/commands/project/ProjectCommand.java
@@ -34,11 +34,13 @@ import imagej.command.Command;
 import imagej.ops.AbstractFunction;
 import imagej.ops.OpService;
 import imagej.ops.descriptors.statistics.Mean;
+import imagej.ops.descriptors.statistics.rt.MeanIRT;
 import imagej.ops.project.Project;
 import net.imglib2.img.Img;
 import net.imglib2.meta.ImgPlus;
 import net.imglib2.meta.TypedAxis;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.DoubleType;
 
 import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
@@ -67,27 +69,29 @@ public class ProjectCommand<T extends RealType<T>> implements Command {
 	@Override
 	public void run() {
 		if (out == null) {
-			final Img<T> img = in.factory().create(in, in.firstElement().createVariable());
+			final Img<T> img = in.factory().create(in,
+					in.firstElement().createVariable());
 			out = new ImgPlus<T>(img, in);
 		}
 		final int axisIndex = in.dimensionIndex(axis.type());
 		ops.run(Project.class, out, in, method, axisIndex);
 	}
 
-	/* -- Wrapper classes to mark certain operations as projection methods --*/
+	/* -- Wrapper classes to mark certain operations as projection methods -- */
 
-	private class ProjectMean extends AbstractFunction<Iterable<T>, T> implements
-		ProjectMethod<T>
-	{
+	private class ProjectMean extends AbstractFunction<Iterable<T>, T>
+			implements ProjectMethod<T> {
 
-		private Mean<Iterable<T>, T> mean;
+		private MeanIRT mean;
 
 		@Override
 		public T compute(final Iterable<T> input, final T output) {
 			if (mean == null) {
-				mean = (Mean<Iterable<T>, T>) ops.op(Mean.class, output, input);
+				mean = (MeanIRT) ops.op(MeanIRT.class, output, input);
 			}
-			return mean.compute(input, output);
+			DoubleType compute = mean.compute(input, new DoubleType());
+			output.setReal(compute.get());
+			return output;
 		}
 
 	}
