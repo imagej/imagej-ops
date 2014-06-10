c8dcf924061e1dab85f7b7374b074cfd26110631
dietzc <christian.dietz@uni-konstanz.de>
2014-06-10 12:19:45 +0200

    Introduces Descriptors based on II instead of IROI

---
+++ src/main/java/net/imagej/ops/descriptors/geometric/ii/EccentricityII.java
@@ -0,0 +1,94 @@
+/*
+ * #%L
+ * ImageJ OPS: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.descriptors.geometric.ii;
+
+import net.imagej.ops.AbstractFunction;
+import net.imagej.ops.Op;
+import net.imagej.ops.descriptors.geometric.Eccentricity;
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.type.numeric.real.DoubleType;
+
+import org.scijava.plugin.Plugin;
+
+/**
+ * Calculating {@link Eccentricity} on {@link Iterable}
+ * 
+ * @author Christian Dietz (University Konstanz)
+ * @author Andreas Graumann (University Konstanz)
+ */
+@Plugin(type = Op.class, name = Eccentricity.NAME, label = Eccentricity.LABEL, priority = -1.0)
+public class EccentricityII extends
+		AbstractFunction<IterableInterval<?>, DoubleType> implements
+		Eccentricity {
+
+	@Override
+	public DoubleType compute(final IterableInterval<?> input,
+			final DoubleType output) {
+
+		final Cursor<?> cursor = input.localizingCursor();
+
+		final int d = cursor.numDimensions();
+		final long[] c1 = new long[d];
+		final long[] c2 = new long[d];
+
+		for (int i = 0; i < d; i++) {
+			c1[i] = Integer.MAX_VALUE;
+			c2[i] = -Integer.MAX_VALUE;
+		}
+
+		// get corners of bounding box
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			for (int dim = 0; dim < d; dim++) {
+				final int pos = cursor.getIntPosition(dim);
+				c1[dim] = (c1[dim] > pos) ? pos : c1[dim];
+				c2[dim] = (c2[dim] < pos) ? pos : c2[dim];
+			}
+		}
+
+		final long[] length = new long[d];
+		for (int dim = 0; dim < 2; dim++) {
+			length[dim] = Math.abs(c1[dim] - c2[dim]);
+		}
+
+		double res = 0;
+		if (length[0] > length[1]) {
+			res = length[0] / length[1];
+		} else {
+			res = length[1] / length[0];
+		}
+
+		output.set(res);
+
+		return output;
+	}
+}
---
+++ src/main/java/net/imagej/ops/descriptors/geometric/ii/FeretII.java
@@ -0,0 +1,101 @@
+/*
+ * #%L
+ * ImageJ OPS: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.descriptors.geometric.ii;
+
+import net.imagej.ops.AbstractFunction;
+import net.imagej.ops.Op;
+import net.imagej.ops.descriptors.geometric.Feret;
+import net.imagej.ops.descriptors.geometric.FeretResult;
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.Point;
+
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Op.class, name = Feret.NAME, label = Feret.LABEL)
+public class FeretII extends AbstractFunction<IterableInterval<?>, FeretResult>
+		implements Feret {
+
+	@Override
+	public FeretResult compute(final IterableInterval<?> input,
+			final FeretResult output) {
+
+		double maxDiameter = 0.0f;
+
+		final Point maxP1 = new Point(input.numDimensions());
+		final Point maxP2 = new Point(input.numDimensions());
+
+		final Cursor<?> cursor = input.localizingCursor();
+
+		final int[] position = new int[cursor.numDimensions()];
+		while (cursor.hasNext()) {
+			cursor.fwd();
+			cursor.localize(position);
+		}
+
+		final Cursor<?> cursor1 = input.localizingCursor();
+
+		// TODO: Is this really correct? Distances are accumulated twice (as p1
+		// + p2 distance and p2 + p1). Can't we avoid this? This is
+		// inefficient!!!!
+		while (cursor1.hasNext()) {
+			cursor1.fwd();
+
+			final Cursor<?> cursor2 = input.localizingCursor();
+			while (cursor2.hasNext()) {
+				cursor2.fwd();
+
+				double dist = 0.0f;
+				for (int i = 0; i < cursor1.numDimensions(); i++) {
+					dist += (cursor1.getIntPosition(i) - cursor2
+							.getIntPosition(i))
+							* (cursor1.getIntPosition(i) - cursor2
+									.getIntPosition(i));
+				}
+
+				if (dist > maxDiameter) {
+					maxDiameter = dist;
+					maxP1.setPosition(cursor1);
+					maxP2.setPosition(cursor2);
+				}
+			}
+		}
+
+		// sqrt for euclidean
+		maxDiameter = Math.sqrt(maxDiameter);
+
+		output.max = maxDiameter;
+		output.p1 = maxP1;
+		output.p2 = maxP2;
+
+		return output;
+	}
+}
