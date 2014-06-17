827892afcfafc4bf75631790a599b8a6a522f87b
DanielSeebacher <danielseebacher@t-online.de>
2014-06-17 12:37:00 +0200

    Tests for Geometric Descriptors added

--- src/test/java/net/imagej/ops/DescriptorServiceTest.java
+++ src/test/java/net/imagej/ops/DescriptorServiceTest.java
@@ -1,6 +1,7 @@
 package net.imagej.ops;
 
 import net.imagej.ops.descriptors.DescriptorService;
+import net.imagej.ops.descriptors.descriptorsets.GeometricDescriptorSet;
 import net.imagej.ops.descriptors.descriptorsets.HaralickDescriptorSet;
 import net.imagej.ops.descriptors.descriptorsets.HistogramDescriptorSet;
 import net.imagej.ops.descriptors.descriptorsets.ZernikeDescriptorSet;
@@ -18,106 +19,127 @@ import org.scijava.module.ModuleException;
 
 public class DescriptorServiceTest extends AbstractOpTest {
 
-	private Img<UnsignedByteType> in;
+    private Img<UnsignedByteType> in;
 
-	private Img<UnsignedByteType> in2;
+    private Img<UnsignedByteType> in2;
 
-	@Before
-	public void init() {
-		in = generateUnsignedByteTestImg(true, 10, 10);
-		in2 = generateUnsignedByteTestImg(true, 100, 100);
+    @Before
+    public void init() {
+	in = generateUnsignedByteTestImg(true, 10, 10);
+	in2 = generateUnsignedByteTestImg(true, 100, 100);
+    }
+
+    /** Subclasses can override to create a context with different services. */
+    @Override
+    protected Context createContext() {
+	return new Context(OpService.class, OpMatchingService.class,
+		DescriptorService.class);
+    }
+
+    // @Test
+    // public void firstOrderStatisticsTest() {
+    //
+    // final FirstOrderStatisticsSet set = new FirstOrderStatisticsSet(context);
+    //
+    // // make it ready for
+    // set.compileFor(in.getClass());
+    //
+    // // First image
+    // set.update(in);
+    //
+    // for (final Pair<String, Double> res : set) {
+    // System.out.println("First: " + res.getA() + " " + res.getB());
+    // }
+    //
+    // // Second image
+    // set.update(in2);
+    // for (final Pair<String, Double> res : set) {
+    // System.out.println("Second: " + res.getA() + " " + res.getB());
+    // }
+    //
+    // }
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void haralickTest() throws IllegalArgumentException, ModuleException {
+
+	final HaralickDescriptorSet<IterableInterval> desc = new HaralickDescriptorSet<IterableInterval>(
+		context, IterableInterval.class);
+
+	desc.compile();
+	desc.update(in);
+
+	desc.updateParameterNrGrayLevels(32);
+	desc.updateParameterDistance(1);
+	desc.updateParameterOrientation(MatrixOrientation.VERTICAL);
+
+	System.out.println("### FIRST RUN ####");
+	for (final Pair<String, DoubleType> res : desc) {
+	    System.out.println(res.getA() + " " + res.getB().get());
 	}
 
-	/** Subclasses can override to create a context with different services. */
-	@Override
-	protected Context createContext() {
-		return new Context(OpService.class, OpMatchingService.class,
-				DescriptorService.class);
-	}
+	System.out.println("\n");
+	desc.updateParameterNrGrayLevels(32);
+	desc.updateParameterDistance(1);
+	desc.updateParameterOrientation(MatrixOrientation.HORIZONTAL);
 
-	// @Test
-	// public void firstOrderStatisticsTest() {
-	//
-	// final FirstOrderStatisticsSet set = new FirstOrderStatisticsSet(context);
-	//
-	// // make it ready for
-	// set.compileFor(in.getClass());
-	//
-	// // First image
-	// set.update(in);
-	//
-	// for (final Pair<String, Double> res : set) {
-	// System.out.println("First: " + res.getA() + " " + res.getB());
-	// }
-	//
-	// // Second image
-	// set.update(in2);
-	// for (final Pair<String, Double> res : set) {
-	// System.out.println("Second: " + res.getA() + " " + res.getB());
-	// }
-	//
-	// }
+	System.out.println("### SECOND RUN ####");
+	for (final Pair<String, DoubleType> res : desc) {
+	    System.out.println(res.getA() + " " + res.getB().get());
+	}
+    }
+
+    @Test
+    public void histogramTest() throws IllegalArgumentException,
+    ModuleException {
+
+	final HistogramDescriptorSet<IterableInterval> desc = new HistogramDescriptorSet<IterableInterval>(
+		context, IterableInterval.class);
+
+	desc.compile();
+
+	desc.updateNumBins(10);
+	desc.update(in);
+
+	for (final Pair<String, DoubleType> res : desc) {
+	    System.out.println("First: " + res.getA() + " " + res.getB().get());
+	}
+    }
+
+    @Test
+    public void zernikeTest() throws IllegalArgumentException, ModuleException {
 
 	@SuppressWarnings("rawtypes")
-	@Test
-	public void haralickTest() throws IllegalArgumentException, ModuleException {
+	final ZernikeDescriptorSet<IterableInterval> desc = new ZernikeDescriptorSet<IterableInterval>(
+		context, IterableInterval.class);
+	desc.compile();
 
-		final HaralickDescriptorSet<IterableInterval> desc = new HaralickDescriptorSet<IterableInterval>(
-				context, IterableInterval.class);
+	desc.updateOrder(1);
+	desc.update(in);
 
-		desc.compile();
-		desc.update(in);
-
-		desc.updateParameterNrGrayLevels(32);
-		desc.updateParameterDistance(1);
-		desc.updateParameterOrientation(MatrixOrientation.VERTICAL);
-
-		System.out.println("### FIRST RUN ####");
-		for (final Pair<String, DoubleType> res : desc) {
-			System.out.println(res.getA() + " " + res.getB().get());
-		}
-
-		System.out.println("\n");
-		desc.updateParameterNrGrayLevels(32);
-		desc.updateParameterDistance(1);
-		desc.updateParameterOrientation(MatrixOrientation.HORIZONTAL);
-
-		System.out.println("### SECOND RUN ####");
-		for (final Pair<String, DoubleType> res : desc) {
-			System.out.println(res.getA() + " " + res.getB().get());
-		}
+	for (final Pair<String, DoubleType> res : desc) {
+	    System.out.println(res.getA() + " " + res.getB().get());
 	}
+    }
 
-	@Test
-	public void histogramTest() throws IllegalArgumentException,
-			ModuleException {
+    @SuppressWarnings("rawtypes")
+    @Test
+    public void geometricTest() throws IllegalArgumentException,
+    ModuleException {
+	final GeometricDescriptorSet<IterableInterval> geomDescs = new GeometricDescriptorSet<IterableInterval>(
+		context, IterableInterval.class);
 
-		final HistogramDescriptorSet<IterableInterval> desc = new HistogramDescriptorSet<IterableInterval>(
-				context, IterableInterval.class);
+	geomDescs.compile();
 
-		desc.compile();
+	geomDescs.update(in);
 
-		desc.updateNumBins(10);
-		desc.update(in);
+	// FIXME: cannot cast results to DoubleType (polygon, feretresult,
+	// double[], etc)
+	// for (final Pair<String, DoubleType> pair : geomDescs) {
+	// System.out.println(pair.getA() + " " + pair.getB().get());
+	// }
 
-		for (final Pair<String, DoubleType> res : desc) {
-			System.out.println("First: " + res.getA() + " " + res.getB().get());
-		}
-	}
+	geomDescs.update(in2);
 
-	@Test
-	public void zernikeTest() throws IllegalArgumentException, ModuleException {
-
-		@SuppressWarnings("rawtypes")
-		final ZernikeDescriptorSet<IterableInterval> desc = new ZernikeDescriptorSet<IterableInterval>(
-				context, IterableInterval.class);
-		desc.compile();
-
-		desc.updateOrder(1);
-		desc.update(in);
-
-		for (final Pair<String, DoubleType> res : desc) {
-			System.out.println(res.getA() + " " + res.getB().get());
-		}
-	}
+    }
 }
