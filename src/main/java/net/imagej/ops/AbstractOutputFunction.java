84a4b2b87278834d3d26ab1ef7c007e26e6407e0
dietzc <christian.dietz@uni-konstanz.de>
2014-08-19 10:34:54 +0200

    Added AbstractOutputFunction
    AbstractOutputFunction provides an implementation of an OutputFunction.
    Especially it implements the compute(I input) method which automatically
    creates an output, if no output has been set, yet.

---
+++ src/main/java/net/imagej/ops/AbstractOutputFunction.java
@@ -0,0 +1,66 @@
+/*
+ * #%L
+ * ImageJ OPS: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops;
+
+/**
+ * Abstract superclass for a {@link OutputFunction} which is capable creating an
+ * output object.
+ * 
+ * @author Christian Dietz (University of Konstanz)
+ */
+public abstract class AbstractOutputFunction<I, O> extends
+		AbstractFunction<I, O> implements OutputFunction<I, O> {
+
+	private OutputFactory<I, O> outputFactory;
+
+	@Override
+	public void run() {
+		// we may not reuse the already created output
+		if (getOutput() == null) {
+			setOutput(compute(getInput()));
+		} else {
+			setOutput(compute(getInput(), getOutput()));
+		}
+	}
+
+	@Override
+	public O compute(final I input) {
+		if (outputFactory == null)
+			outputFactory = getOutputFactory();
+
+		return compute(input, getOutputFactory().create(input));
+	}
+
+	@Override
+	public void setOutputFactory(final OutputFactory<I, O> outputFactory) {
+		this.outputFactory = outputFactory;
+	}
+}
