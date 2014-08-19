4d3f8688c796f636289b8f875d3a84e9bdf8cea2
dietzc <christian.dietz@uni-konstanz.de>
2014-08-19 10:02:43 +0200

    Added OutputFunction interface
    An OutputFunction is a Function, which can provide the output object on
    its own given some input object.

---
+++ src/main/java/net/imagej/ops/OutputFunction.java
@@ -0,0 +1,58 @@
+/*
+ * #%L
+ * ImageJ OPS: a framework for reusable algorithms.
+ * %%
+ * Copyright (C) 2014 Board of Regents of the University of
+ * Wisconsin-Madison and University of Konstanz.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops;
+
+/**
+ * An {@link OutputFunction} is an {@link Function} which is able to create the
+ * output object itself.
+ * 
+ * @author Christian Dietz (University of Konstanz)
+ */
+public interface OutputFunction<I, O> extends InputOp<I>, OutputOp<O>,
+		Threadable {
+
+	/**
+	 * Compute the output of a function, given some input.
+	 * 
+	 * @param input
+	 *            of the {@link OutputFunction}
+	 * 
+	 * @return output
+	 */
+	O compute(I input);
+
+	/**
+	 * @return create an output object of type O, given some input. The output
+	 *         can then be used to call compute(I input, O output), which will
+	 *         fill the output with the result.
+	 */
+	O createOutput(I input);
+}
