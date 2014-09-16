e3db0edabf49b1d70d81c57093c169f7609e09c8
dietzc <christian.dietz@uni-konstanz.de>
2014-09-16 17:56:30 -0500

    ConvertClip: Add JavaDoc

--- src/main/java/net/imagej/ops/convert/ConvertClip.java
+++ src/main/java/net/imagej/ops/convert/ConvertClip.java
@@ -12,7 +12,13 @@ import net.imagej.ops.Function;
  * @Plugin(type = Op.class, name = ConvertClip.NAME)
  * </pre>
  * 
- * @author Christian Dietz
+ * <p>
+ * Converts some <I> to another <O>. The values are copied. If a value of the
+ * input is lower/higher than the output minimum/maximum, it will be set to the
+ * minimum/maximum of the output.
+ * </p>
+ * 
+ * @author Christian Dietz (University of Konstanz)
  */
 public interface ConvertClip<I, O> extends Function<I, O> {
 	String NAME = "convert-clip";
