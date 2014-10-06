80a0609c8a99231bc201b825e7d38c0b6145671f
dietzc <christian.dietz@uni-konstanz.de>
2014-10-06 18:48:15 +0200

    add default implementations of area for iterableintervals

---
+++ src/main/java/net/imagej/ops/statistics/geometric/AreaII.java
@@ -0,0 +1,10 @@
+package net.imagej.ops.statistics.geometric;
+
+import net.imagej.ops.OutputFunction;
+import net.imglib2.IterableInterval;
+import net.imglib2.type.numeric.RealType;
+
+public interface AreaII extends
+		OutputFunction<IterableInterval<?>, RealType<?>> {
+
+}
