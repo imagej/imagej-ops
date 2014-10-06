26a9b054bf31a0b0d4f38be4fc65a946486b48af
dietzc <christian.dietz@uni-konstanz.de>
2014-10-06 18:47:52 +0200

    update set of haralick features and ops
    * add new implementations of features and ops
    * remove old implementations
    * add templates for tests
    * add featureset

---
+++ src/main/java/net/imagej/ops/features/haralick/DefHaralickConstrastFeature.java
@@ -0,0 +1,40 @@
+package net.imagej.ops.features.haralick;
+
+import net.imagej.ops.Op;
+import net.imagej.ops.features.haralick.HaralickFeatures.HaralickContrastFeature;
+import net.imagej.ops.features.haralick.helper.CoocPXMinusY;
+import net.imagej.ops.features.haralick.helper.CooccurrenceMatrix;
+
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Op.class, label = "Haralick2D: Contrast")
+public class DefHaralickConstrastFeature implements HaralickContrastFeature {
+
+	@Parameter
+	private CooccurrenceMatrix matrix;
+
+	@Parameter
+	private CoocPXMinusY coocPXMinusY;
+
+	@Parameter(type = ItemIO.OUTPUT)
+	private double output;
+	
+	@Override
+	public void run() {
+		final int nrGrayLevels = matrix.getNrGreyLevels();
+		final double[] pxminusxy = coocPXMinusY.getOutput();
+
+		output = 0;
+		for (int k = 0; k <= nrGrayLevels - 1; k++) {
+			output += k * k * pxminusxy[k];
+		}
+	}
+
+	@Override
+	public double getFeatureValue() {
+		return output;
+	}
+
+}
