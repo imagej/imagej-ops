b50b058da51f67da66fd23bdb284df1e4d32024f
Curtis Rueden <ctrueden@wisc.edu>
2015-01-16 17:04:11 -0600

    Update license headers and project license
    This adds Brian Northan as an author, updates the year range to 2015,
    and adds missing license blurbs as needed.
    
    Accomplished using:
    
        mvn license:update-file-header
        mvn license:update-project-license

--- src/main/java/net/imagej/ops/threshold/global/ThresholdUtils.java
+++ src/main/java/net/imagej/ops/threshold/global/ThresholdUtils.java
@@ -2,9 +2,8 @@
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2009 - 2014 Board of Regents of the University of
- * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
- * Institute of Molecular Cell Biology and Genetics.
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
--- src/main/java/net/imglib2/ops/operation/iterableinterval/unary/MinMax.java
+++ src/main/java/net/imglib2/ops/operation/iterableinterval/unary/MinMax.java
@@ -4,8 +4,8 @@ package net.imglib2.ops.operation.iterableinterval.unary;
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2014 Board of Regents of the University of
- * Wisconsin-Madison and University of Konstanz.
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
