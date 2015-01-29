7be9069c0dd50d85dd0d64677dc33df6b5717002
dietzc <christian.dietz@uni-konstanz.de>
2015-01-29 21:30:21 +0100

    add more convenience methods to OpResolverService

--- src/main/java/net/imagej/ops/features/OpResolverService.java
+++ src/main/java/net/imagej/ops/features/OpResolverService.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import net.imagej.ops.Op;
 import net.imagej.ops.OpRef;
+import net.imagej.ops.OutputOp;
 
 import org.scijava.service.Service;
 
@@ -16,6 +17,13 @@ public interface OpResolverService extends Service {
 	<I, OP extends Op> ResolvedOpSet<I> build(final I input,
 			final Class<OP> type, final Object... args);
 
-	<I, OP extends Op> ResolvedOpSet<I> build(final I input, OpRef<?> ... refs);
+	<I> ResolvedOpSet<I> build(final I input, OpRef<?>... refs);
 
+	<I, O> ResolvedOp<I, O> resolveAndRun(final Class<O> outType,
+			final I input,
+			@SuppressWarnings("rawtypes") final Class<? extends OutputOp> type,
+			final Object... args);
+
+	<I, O> ResolvedOp<I, O> resolveAndRun(final Class<O> outType, final I input,
+			@SuppressWarnings("rawtypes") final OpRef<? extends OutputOp> ref);
 }
