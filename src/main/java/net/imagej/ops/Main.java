ff60555a62974920c0e2592962d978504ae25c15
Curtis Rueden <ctrueden@wisc.edu>
2015-02-22 12:20:57 -0600

    Add a main class using the OPS-driven evaluator

---
+++ src/main/java/net/imagej/ops/Main.java
@@ -0,0 +1,60 @@
+/*
+ * #%L
+ * SciJava mathematical expression parser.
+ * %%
+ * Copyright (C) 2015 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops;
+
+import java.io.IOException;
+
+import net.imagej.ops.eval.OpEvaluator;
+
+import org.scijava.Context;
+import org.scijava.sjep.eval.EvaluatorConsole;
+
+/**
+ * Launches a console-based, OPS-driven expression evaluator.
+ *
+ * @author Curtis Rueden
+ * @see EvaluatorConsole
+ */
+public final class Main {
+
+	private Main() {
+		// Prevent instantiation of utility class.
+	}
+
+	// -- Main method --
+
+	public static void main(final String[] args) throws IOException {
+		final Context context = new Context(OpService.class);
+		final OpService ops = context.getService(OpService.class);
+		new EvaluatorConsole(new OpEvaluator(ops)).showConsole();
+	}
+
+}
