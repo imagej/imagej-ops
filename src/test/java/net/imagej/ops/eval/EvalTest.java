d6fb18022329ff012125c51b569cc086023eb778
Curtis Rueden <ctrueden@wisc.edu>
2015-02-21 22:20:05 -0600

    Add a simple unit test for eval op

---
+++ src/test/java/net/imagej/ops/eval/EvalTest.java
@@ -0,0 +1,65 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.eval;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.imagej.ops.AbstractOpTest;
+import net.imagej.ops.Ops.Eval;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link Eval}.
+ * 
+ * @author Curtis Rueden
+ */
+public class EvalTest extends AbstractOpTest {
+
+	@Test
+	public void testMath() {
+		final Map<String, Object> vars = new HashMap<String, Object>();
+		vars.put("a", 2);
+		vars.put("b", 3);
+		vars.put("c", 5);
+
+		assertEquals(7, ops.eval("a+c", vars));
+		assertEquals(3, ops.eval("c-a", vars));
+		assertEquals(6, ops.eval("a*b", vars));
+		assertEquals(2, ops.eval("c/a", vars));
+		assertEquals(1, ops.eval("c%a", vars));
+		assertEquals(17, ops.eval("a+b*c", vars));
+	}
+
+}
