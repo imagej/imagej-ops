c8639acef4124e745b554c64ead6166484789627
Brian Northan <bnorthan@gmail.com>
2015-04-28 07:25:11 -0400

    Add ConvolveNaiveImg

---
+++ src/main/java/net/imagej/ops/convolve/ConvolveNaiveImg.java
@@ -0,0 +1,95 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.convolve;
+
+import net.imagej.ops.Contingent;
+import net.imagej.ops.Op;
+import net.imagej.ops.Ops;
+import net.imagej.ops.fft.filter.AbstractFilterImg;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.outofbounds.OutOfBoundsConstantValueFactory;
+import net.imglib2.outofbounds.OutOfBoundsFactory;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.Util;
+import net.imglib2.view.Views;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Convolves an image naively (no FFTs).
+ */
+@Plugin(type = Op.class, name = Ops.Convolve.NAME,
+	priority = Priority.HIGH_PRIORITY)
+public class ConvolveNaiveImg<I extends RealType<I>, O extends RealType<O>, K extends RealType<K>>
+	extends AbstractFilterImg<I, O, K> implements Contingent, Ops.Convolve
+{
+
+	protected Img<O> safeCompute(Img<I> img, Img<O> out) {
+
+		if (obfInput == null) {
+			obfInput =
+				new OutOfBoundsConstantValueFactory<I, RandomAccessibleInterval<I>>(
+					Util.getTypeFromInterval(img).createVariable());
+		}
+
+		if ((obfKernel == null) && (kernel != null)) {
+			obfKernel =
+				new OutOfBoundsConstantValueFactory<K, RandomAccessibleInterval<K>>(
+					Util.getTypeFromInterval(kernel).createVariable());
+		}
+
+		// extend the input
+		RandomAccessibleInterval<I> extendedIn =
+			Views.interval(Views.extend(img, obfInput), img);
+
+		OutOfBoundsFactory<O, RandomAccessibleInterval<O>> obfOutput =
+			new OutOfBoundsConstantValueFactory<O, RandomAccessibleInterval<O>>(Util
+				.getTypeFromInterval(out).createVariable());
+
+		// extend the output
+		RandomAccessibleInterval<O> extendedOut =
+			Views.interval(Views.extend(out, obfOutput), out);
+
+		ops.run(ConvolveNaive.class, extendedOut, extendedIn, kernel);
+
+		return out;
+	}
+
+	@Override
+	public boolean conforms() {
+		// conforms only if the kernel is sufficiently small
+		return Intervals.numElements(kernel) <= 9;
+	}
+
+}
