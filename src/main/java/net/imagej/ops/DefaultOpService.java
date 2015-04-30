d7f077294778ace175209b53f70cc31960fed784
Curtis Rueden <ctrueden@wisc.edu>
2015-04-30 16:43:16 -0500

    OpService: remove non-global ops
    The add, divide, multiply and subtract ops are now part of the math
    namespace. They are now accessible via the math gateway instead; e.g.:
    
      ops.math().add(...).

--- src/main/java/net/imagej/ops/DefaultOpService.java
+++ src/main/java/net/imagej/ops/DefaultOpService.java
@@ -382,11 +382,6 @@ public class DefaultOpService extends AbstractPTService<Op> implements
 	}
 
 	@Override
-	public Object multiply(final Object... args) {
-		return run(MathOps.Multiply.NAME, args);
-	}
-
-	@Override
 	public Object normalize(final Object... args) {
 		return run(Ops.Normalize.NAME, args);
 	}
