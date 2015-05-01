8a80e99563d63980afb672ef3b73b22a1edef43c
Curtis Rueden <ctrueden@wisc.edu>
2015-05-01 17:41:35 -0500

    LogicNamespace: add type-safe equal signatures

--- src/main/java/net/imagej/ops/logic/LogicNamespace.java
+++ src/main/java/net/imagej/ops/logic/LogicNamespace.java
@@ -7,13 +7,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -35,7 +35,7 @@ import net.imagej.ops.OpMethod;
 
 /**
  * The logic namespace contains logical (i.e., boolean) operations.
- * 
+ *
  * @author Curtis Rueden
  */
 public class LogicNamespace extends AbstractNamespace {
@@ -47,6 +47,53 @@ public class LogicNamespace extends AbstractNamespace {
 		final boolean result =
 			(Boolean) ops().run(net.imagej.ops.logic.PrimitiveLogic.BooleanAnd.class,
 				a, b);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.logic.PrimitiveLogic.BooleanEqual.class)
+	public boolean equal(final boolean a, final boolean b) {
+		final boolean result =
+			(Boolean) ops().run(
+				net.imagej.ops.logic.PrimitiveLogic.BooleanEqual.class, a, b);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.logic.PrimitiveLogic.IntegerEqual.class)
+	public boolean equal(final int a, final int b) {
+		final boolean result =
+			(Boolean) ops().run(
+				net.imagej.ops.logic.PrimitiveLogic.IntegerEqual.class, a, b);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.logic.PrimitiveLogic.LongEqual.class)
+	public boolean equal(final long a, final long b) {
+		final boolean result =
+			(Boolean) ops().run(net.imagej.ops.logic.PrimitiveLogic.LongEqual.class,
+				a, b);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.logic.PrimitiveLogic.FloatEqual.class)
+	public boolean equal(final float a, final float b) {
+		final boolean result =
+			(Boolean) ops().run(net.imagej.ops.logic.PrimitiveLogic.FloatEqual.class,
+				a, b);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.logic.PrimitiveLogic.DoubleEqual.class)
+	public boolean equal(final double a, final double b) {
+		final boolean result =
+			(Boolean) ops().run(
+				net.imagej.ops.logic.PrimitiveLogic.DoubleEqual.class, a, b);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.logic.ObjectsEqual.class)
+	public boolean equal(final Object a, final Object b) {
+		final boolean result =
+			(Boolean) ops().run(net.imagej.ops.logic.ObjectsEqual.class, a, b);
 		return result;
 	}
 
