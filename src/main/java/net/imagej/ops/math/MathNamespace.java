3304b757c9e334ccdd99ff5e6c49e15c31c18c5d
Alison Walter <awalter2@wisc.edu>
2015-05-29 16:22:21 -0500

    Add implementation for Arccot built-in

--- src/main/java/net/imagej/ops/math/MathNamespace.java
+++ src/main/java/net/imagej/ops/math/MathNamespace.java
@@ -347,6 +347,16 @@ public class MathNamespace extends AbstractNamespace {
 		return ops().run(net.imagej.ops.MathOps.Arccot.class, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.arithmetic.real.RealArccot.class)
+	public <I extends RealType<I>, O extends RealType<O>> O arccot(final O out,
+		final I in)
+	{
+		@SuppressWarnings("unchecked")
+		final O result =
+			(O) ops().run(net.imagej.ops.arithmetic.real.RealArccot.class, out, in);
+		return result;
+	}
+
 	@OpMethod(op = net.imagej.ops.MathOps.Arccoth.class)
 	public Object arccoth(final Object... args) {
 		return ops().run(net.imagej.ops.MathOps.Arccoth.class, args);
