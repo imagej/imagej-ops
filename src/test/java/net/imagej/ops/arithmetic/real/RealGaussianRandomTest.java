a8b0c0fdfb4f60371d65c1629b1760334786695f
Alison Walter <awalter2@wisc.edu>
2015-06-11 17:04:40 -0500

    Add test for RealGaussianRandom functionality

---
+++ src/test/java/net/imagej/ops/arithmetic/real/RealGaussianRandomTest.java
@@ -0,0 +1,84 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.arithmetic.real;
+
+import static org.junit.Assert.assertEquals;
+import net.imagej.ops.AbstractOpTest;
+import net.imglib2.type.numeric.real.DoubleType;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link RealGaussianRandom}.
+ *
+ * @author Alison Walter
+ * @author Curtis Rueden
+ */
+public class RealGaussianRandomTest extends AbstractOpTest {
+
+	@Test
+	public void testGaussianRandom() {
+		assertGaussianRandom(23, 16.53373419964066);
+		assertGaussianRandom(27, -15.542815799078497, 0xfeeddeadbeefbeefL);
+		assertGaussianRandom(123, -49.838353142718006, 124, 181.75101003563117);
+	}
+
+	private void assertGaussianRandom(final double i, final double o) {
+		final DoubleType in = new DoubleType(i);
+		final DoubleType out = ops.math().gaussianrandom(in.createVariable(), in);
+		assertEquals(o, out.get(), 0);
+	}
+
+	private void assertGaussianRandom(final double i, final double o,
+		final long seed)
+	{
+		final DoubleType in = new DoubleType(i);
+		final DoubleType out =
+			ops.math().gaussianrandom(in.createVariable(), in, seed);
+		assertEquals(o, out.get(), 0);
+	}
+
+	private void assertGaussianRandom(final double i, final double o,
+		final double i2, final double o2)
+	{
+		final DoubleType in = new DoubleType(i);
+		final DoubleType out = new DoubleType();
+		final long seed = 0xcafebabe12345678L;
+		@SuppressWarnings("unchecked")
+		final RealGaussianRandom<DoubleType, DoubleType> op =
+			ops.op(RealGaussianRandom.class, in.createVariable(), in, seed);
+		op.compute(in, out);
+		assertEquals(o, out.get(), 0);
+		in.set(i2);
+		op.compute(in, out);
+		assertEquals(o2, out.get(), 0);
+	}
+}
