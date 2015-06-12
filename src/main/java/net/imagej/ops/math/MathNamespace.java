54166c5e29909358d92eb46de7a40751f3b5e97c
Alison Walter <awalter2@wisc.edu>
2015-06-12 16:46:44 -0500

    Add implementation for Sinh built-in

--- src/main/java/net/imagej/ops/math/MathNamespace.java
+++ src/main/java/net/imagej/ops/math/MathNamespace.java
@@ -1459,6 +1459,23 @@ public class MathNamespace extends AbstractNamespace {
 		return ops().run(net.imagej.ops.MathOps.Sinh.class, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.math.PrimitiveMath.DoubleSinh.class)
+	public double sinh(final double a) {
+		final double result =
+			(Double) ops().run(net.imagej.ops.math.PrimitiveMath.DoubleSinh.class, a);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.arithmetic.real.RealSinh.class)
+	public <I extends RealType<I>, O extends RealType<O>> O sinh(final O out,
+		final I in)
+	{
+		@SuppressWarnings("unchecked")
+		final O result =
+			(O) ops().run(net.imagej.ops.arithmetic.real.RealSinh.class, out, in);
+		return result;
+	}
+
 	@OpMethod(op = net.imagej.ops.MathOps.Sqr.class)
 	public Object sqr(final Object... args) {
 		return ops().run(net.imagej.ops.MathOps.Sqr.class, args);
