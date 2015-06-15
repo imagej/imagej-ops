eed2f5539789e63ea6b34d1a2ae84248dd07e0e5
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-15 19:08:15 +0200

    Rename view to viewOp

--- src/main/java/net/imagej/ops/view/DefaultView.java
+++
@@ -1,8 +0,0 @@
-package net.imagej.ops.view;
-
-import net.imagej.ops.OutputOp;
-import net.imagej.ops.ViewOps.View;
-
-public interface DefaultView<O> extends View, OutputOp<O> {
-	// NB: Marker interface
-}
