5e2170f81605994580222ce32603bc1196db095b
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-15 16:54:43 +0200

    Add test for Interval op.

---
+++ src/test/java/net/imagej/ops/view/IntervalTest.java
@@ -0,0 +1,23 @@
+package net.imagej.ops.view;
+
+import static org.junit.Assert.assertTrue;
+import net.imagej.ops.AbstractOpTest;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.view.IntervalView;
+
+import org.junit.Test;
+
+public class IntervalTest extends AbstractOpTest {
+
+	@Test
+	public void testInterval() {
+		Img<DoubleType> img = new ArrayImgFactory<DoubleType>().create(
+				new int[] { 10, 100 }, new DoubleType());
+		
+		IntervalView<DoubleType> view = (IntervalView<DoubleType>) ops.run(Interval.class, img);
+	
+		assertTrue(view.getSource().equals(img));
+	}
+}
