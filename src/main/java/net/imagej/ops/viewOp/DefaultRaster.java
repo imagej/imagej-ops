bf841103a80fb2a3e599f020ab65dd141a3d30ef
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-17 18:48:15 +0200

    Add default implementation of Raster interface.

---
+++ src/main/java/net/imagej/ops/viewOp/DefaultRaster.java
@@ -0,0 +1,25 @@
+package net.imagej.ops.viewOp;
+
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.view.ViewOps;
+import net.imagej.ops.view.ViewOps.Raster;
+import net.imglib2.RealRandomAccessible;
+import net.imglib2.type.Type;
+import net.imglib2.view.RandomAccessibleOnRealRandomAccessible;
+import net.imglib2.view.Views;
+
+@Plugin(type = ViewOps.Raster.class, name = ViewOps.Raster.NAME)
+public class DefaultRaster<T extends Type<T>>
+		extends
+		AbstractView<RealRandomAccessible<T>, RandomAccessibleOnRealRandomAccessible<T>>
+		implements
+		Raster<RealRandomAccessible<T>, RandomAccessibleOnRealRandomAccessible<T>> {
+
+	@Override
+	public RandomAccessibleOnRealRandomAccessible<T> compute(
+			RealRandomAccessible<T> input) {
+		return Views.raster(input);
+	}
+
+}
