c95ef4c2fba3f512be5b21007f8cb9f75db20c07
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-17 18:47:41 +0200

    Add default implementation of ExtendRandom interface.

---
+++ src/main/java/net/imagej/ops/viewOp/DefaultExtendRandom.java
@@ -0,0 +1,29 @@
+package net.imagej.ops.viewOp;
+
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.view.ViewOps;
+import net.imagej.ops.view.ViewOps.ExtendRandom;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.view.ExtendedRandomAccessibleInterval;
+import net.imglib2.view.Views;
+
+@Plugin(type = ViewOps.ExtendRandom.class, name = ViewOps.ExtendRandom.NAME)
+public class DefaultExtendRandom<T extends RealType<T>, F extends RandomAccessibleInterval<T>>
+		extends AbstractView<F, ExtendedRandomAccessibleInterval<T, F>>
+		implements ExtendRandom<F, ExtendedRandomAccessibleInterval<T, F>> {
+
+	@Parameter(type = ItemIO.INPUT)
+	private double min;
+
+	@Parameter(type = ItemIO.INPUT)
+	private double max;
+
+	@Override
+	public ExtendedRandomAccessibleInterval<T, F> compute(F input) {
+		return Views.extendRandom(input, min, max);
+	}
+}
