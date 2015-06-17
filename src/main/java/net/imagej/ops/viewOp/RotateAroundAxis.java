110f3573b33b7c6dde5519f58b580d5c2d4059a9
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-17 18:45:10 +0200

    Fix annotation and types.

--- src/main/java/net/imagej/ops/viewOp/RotateAroundAxis.java
+++ src/main/java/net/imagej/ops/viewOp/RotateAroundAxis.java
@@ -1,19 +1,19 @@
 package net.imagej.ops.viewOp;
 
-import net.imagej.ops.Op;
-import net.imagej.ops.ViewOps.View;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.type.Type;
+import net.imagej.ops.view.ViewOps;
+import net.imagej.ops.view.ViewOps.Rotate;
+import net.imglib2.RandomAccessible;
+import net.imglib2.view.MixedTransformView;
 import net.imglib2.view.Views;
 
 import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-@Plugin(type = Op.class, name = View.NAME)
-public class RotateAroundAxis<I extends Type<I>> extends AbstractDefaultView<I>
-		implements
-		Rotate<RandomAccessibleInterval<I>, RandomAccessibleInterval<?>> {
+@Plugin(type = ViewOps.Rotate.class, name = ViewOps.Rotate.NAME)
+public class RotateAroundAxis<T> extends
+		AbstractView<RandomAccessible<T>, MixedTransformView<T>> implements
+		Rotate<RandomAccessible<T>, MixedTransformView<T>> {
 
 	@Parameter(type = ItemIO.INPUT)
 	private int fromAxis;
@@ -22,7 +22,7 @@ public class RotateAroundAxis<I extends Type<I>> extends AbstractDefaultView<I>
 	private int toAxis;
 
 	@Override
-	public RandomAccessibleInterval<?> compute(RandomAccessibleInterval<I> input) {
+	public MixedTransformView<T> compute(RandomAccessible<T> input) {
 		return Views.rotate(input, fromAxis, toAxis);
 	}
 
