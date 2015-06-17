caa360b884d487334acba76c04dcda7471e68626
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-17 18:47:34 +0200

    Add default implementation of ExtendPeriodic interface.

---
+++ src/main/java/net/imagej/ops/viewOp/DefaultExtendPeriodic.java
@@ -0,0 +1,20 @@
+package net.imagej.ops.viewOp;
+
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.view.ViewOps;
+import net.imagej.ops.view.ViewOps.ExtendPeriodic;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.view.ExtendedRandomAccessibleInterval;
+import net.imglib2.view.Views;
+
+@Plugin(type = ViewOps.ExtendPeriodic.class, name = ViewOps.ExtendPeriodic.NAME)
+public class DefaultExtendPeriodic<T, F extends RandomAccessibleInterval<T>>
+		extends AbstractView<F, ExtendedRandomAccessibleInterval<T, F>>
+		implements ExtendPeriodic<F, ExtendedRandomAccessibleInterval<T, F>> {
+
+	@Override
+	public ExtendedRandomAccessibleInterval<T, F> compute(F input) {
+		return Views.extendPeriodic(input);
+	}
+}
