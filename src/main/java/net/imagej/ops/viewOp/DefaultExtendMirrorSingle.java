e057aef6c9b243db6a8024ebb609146f9ae53c14
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-17 18:47:11 +0200

    Add default implementation of ExtendMirrorSingle interface.

---
+++ src/main/java/net/imagej/ops/viewOp/DefaultExtendMirrorSingle.java
@@ -0,0 +1,21 @@
+package net.imagej.ops.viewOp;
+
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.view.ViewOps;
+import net.imagej.ops.view.ViewOps.ExtendMirrorSingle;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.view.ExtendedRandomAccessibleInterval;
+import net.imglib2.view.Views;
+
+@Plugin(type = ViewOps.ExtendMirrorSingle.class, name = ViewOps.ExtendMirrorSingle.NAME)
+public class DefaultExtendMirrorSingle<T, F extends RandomAccessibleInterval<T>>
+		extends AbstractView<F, ExtendedRandomAccessibleInterval<T, F>>
+		implements
+		ExtendMirrorSingle<F, ExtendedRandomAccessibleInterval<T, F>> {
+
+	@Override
+	public ExtendedRandomAccessibleInterval<T, F> compute(F input) {
+		return Views.extendMirrorSingle(input);
+	}
+}
