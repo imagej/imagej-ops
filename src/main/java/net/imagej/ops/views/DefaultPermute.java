4bf9de6135efa489dd817c90427175cafdcd0e8e
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-06-22 11:06:12 +0200

    Add default implementation of Permute interface.

---
+++ src/main/java/net/imagej/ops/views/DefaultPermute.java
@@ -0,0 +1,29 @@
+package net.imagej.ops.views;
+
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.view.ViewOps;
+import net.imagej.ops.view.ViewOps.Permute;
+import net.imglib2.RandomAccessible;
+import net.imglib2.view.MixedTransformView;
+import net.imglib2.view.Views;
+
+@Plugin(type = ViewOps.Permute.class, name = ViewOps.Permute.NAME)
+public class DefaultPermute<T> extends
+		AbstractView<RandomAccessible<T>, MixedTransformView<T>> implements
+		Permute<RandomAccessible<T>, MixedTransformView<T>> {
+
+	@Parameter(type = ItemIO.INPUT)
+	private int fromAxis;
+
+	@Parameter(type = ItemIO.INPUT)
+	private int toAxis;
+
+	@Override
+	public MixedTransformView<T> compute(RandomAccessible<T> input) {
+		return Views.permute(input, fromAxis, toAxis);
+	}
+
+}
