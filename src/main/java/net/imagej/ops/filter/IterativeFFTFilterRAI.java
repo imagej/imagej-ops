ffedf71716288f98d15f04641564f59858680b36
Brian Northan <bnorthan@gmail.com>
2015-06-23 11:43:31 -0400

    implement noncirculant edge handling
    Implement the non-circulant edge handling scheme described here
    
    http://bigwww.epfl.ch/deconvolution/challenge2013/index.html?p=doc_math_rl
    
    The notations we use are slightly different than the notations
    in the above link.  The implementation is based on Matlab scripts
    provided by the organizers of the 2014 Deconvolution Grand Challenge.

--- src/main/java/net/imagej/ops/filter/IterativeFFTFilterRAI.java
+++ src/main/java/net/imagej/ops/filter/IterativeFFTFilterRAI.java
@@ -31,8 +31,12 @@
 package net.imagej.ops.filter;
 
 import net.imagej.ops.OpService;
+import net.imagej.ops.filter.convolve.ConvolveFFTRAI;
+import net.imagej.ops.filter.correlate.CorrelateFFTRAI;
 import net.imglib2.Cursor;
 import net.imglib2.Interval;
+import net.imglib2.Point;
+import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
@@ -44,6 +48,7 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.util.Util;
 import net.imglib2.view.Views;
 
+import org.scijava.Context;
 import org.scijava.plugin.Parameter;
 
 /**
@@ -62,18 +67,52 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 	@Parameter
 	private OpService ops;
 
+	@Parameter
+	Context ctx;
+
 	/**
 	 * Max number of iterations to perform
 	 */
 	@Parameter
 	private int maxIterations;
 
+	/**
+	 * The interval to process
+	 */
 	@Parameter
 	private Interval imgConvolutionInterval;
 
+	/**
+	 * The ImgFactory used to create images
+	 */
 	@Parameter
 	private ImgFactory<O> imgFactory;
 
+	/**
+	 * TODO: review k is the size of the measurement window. That is the size of
+	 * the acquired image before extension k is required to calculate the
+	 * non-circulant normalization factor
+	 */
+	@Parameter(required = false)
+	private long[] k;
+
+	/**
+	 * TODO: review l is the size of the psf. l is required to calculate the
+	 * non-circulant normalization factor
+	 */
+	@Parameter(required = false)
+	private long[] l;
+
+	/**
+	 * TODO: review boolean which indicates wether to perform non-circulant
+	 * deconvolution
+	 */
+	@Parameter(required = false)
+	private boolean nonCirculant = true;
+
+	/**
+	 * TODO: review An OutOfBoundsFactory which defines the extension strategy
+	 */
 	@Parameter(required = false)
 	private OutOfBoundsFactory<O, RandomAccessibleInterval<O>> obfOutput;
 
@@ -82,6 +121,10 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 	private RandomAccessibleInterval<O> raiExtendedEstimate;
 
 	private Img<O> reblurred;
+
+	// Normalization factor for edge handling (see
+	// http://bigwww.epfl.ch/deconvolution/challenge2013/index.html?p=doc_math_rl)
+	private Img<O> normalization = null;
 
 	@Override
 	public void run() {
@@ -97,11 +140,10 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 	 */
 	protected void initialize() {
 
-		// if no output out of bounds factory exists create it
-		if (obfOutput == null) {
-			obfOutput =
-				new OutOfBoundsConstantValueFactory<O, RandomAccessibleInterval<O>>(
-					Util.getTypeFromInterval(getOutput()).createVariable());
+		// if no output out of bounds factory exists create the obf for output
+		if (getObfOutput() == null) {
+			setObfOutput(new OutOfBoundsConstantValueFactory<O, RandomAccessibleInterval<O>>(
+				Util.getTypeFromInterval(getOutput()).createVariable()));
 		}
 
 		Type<O> outType = Util.getTypeFromInterval(getOutput());
@@ -112,13 +154,13 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 		// TODO: review this step
 		// extend the output and use it as a buffer to store the estimate
 		raiExtendedEstimate =
-			Views.interval(Views.extend(getOutput(), obfOutput),
-				imgConvolutionInterval);
+			Views.interval(Views.extend(getOutput(), getObfOutput()),
+				getImgConvolutionInterval());
 
 		// assemble the extended view of the reblurred
 		raiExtendedReblurred =
-			Views
-				.interval(Views.extend(reblurred, obfOutput), imgConvolutionInterval);
+			Views.interval(Views.extend(reblurred, getObfOutput()),
+				getImgConvolutionInterval());
 
 		// perform fft of input
 		ops.filter().fft(getFFTInput(), getRAIExtendedInput());
@@ -126,18 +168,37 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 		// perform fft of psf
 		ops.filter().fft(getFFTKernel(), getRAIExtendedKernel());
 
+		// if non-circulant decon mode create image for normalization
+		if (nonCirculant) {
+			normalization =
+				getImgFactory().create(raiExtendedEstimate, outType.createVariable());
+
+			this.CreateNormalizationImageSemiNonCirculant();
+		}
+
 		// set first guess of estimate
 		// TODO: implement logic for various first guesses.
 		// for now just set to original image
 		Cursor<O> c = Views.iterable(raiExtendedEstimate).cursor();
 		Cursor<I> cIn = Views.iterable(getRAIExtendedInput()).cursor();
 
+		// ops
+		// .run("correlate", normalization, normalization, getRaiExtendedKernel(),
+		// new int[]{0,0,0});
 		while (c.hasNext()) {
 			c.fwd();
 			cIn.fwd();
 			c.get().setReal(cIn.get().getRealFloat());
 		}
 
+		// TODO: see if we still need to unmask values outside fft space
+		/*		// fft space can be slightly larger then the object space so so use a mask
+			// to get
+			// rid of any values outside the object space.
+			// StaticFunctions.InPlaceMultiply(normalization, mask);
+			*/
+
+		//
 		createReblurred();
 
 		// TODO: code for edge handling scheme
@@ -156,7 +217,131 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 			getFFTKernel(), raiExtendedReblurred, true, false);
 	}
 
+	/**
+	 * create the normalization image needed for semi noncirculant model see
+	 * http://bigwww.epfl.ch/deconvolution/challenge2013/index.html?p=doc_math_rl
+	 */
+	protected void CreateNormalizationImageSemiNonCirculant() {
+
+		final RandomAccessibleInterval<O> raiExtendedReblurred =
+			getRAIExtendedReblurred();
+
+		// k is the window size (valid image region)
+		int length = k.length;
+
+		long[] n = new long[length];
+
+		// n is the valid image size plus the extended region
+		// also referred to as object space size
+		for (int d = 0; d < length; d++) {
+			n[d] = raiExtendedReblurred.dimension(d);
+		}
+
+		// create the normalization image
+		final O type = Util.getTypeFromInterval(raiExtendedReblurred);
+		normalization = getImgFactory().create(raiExtendedReblurred, type);
+
+		// size of the measurement window
+		Point size = new Point(3);
+		size.setPosition(k[0], 0);
+		size.setPosition(k[1], 1);
+		size.setPosition(k[2], 2);
+
+		// starting point of the measurement window when it is centered in fft space
+		Point start = new Point(3);
+		start.setPosition((n[0] - k[0]) / 2, 0);
+		start.setPosition((n[1] - k[1]) / 2, 1);
+		start.setPosition((n[2] - k[2]) / 2, 2);
+
+		// draw a cube the size of the measurement space
+		drawCube(normalization, start, size, 1.0);
+
+		// 3. correlate psf with the output of step 2.
+		ops.run(CorrelateFFTRAI.class, normalization, null, getFFTInput(),
+			getFFTKernel(), normalization, true, false);
+
+		try {
+			// saver.saveImg("/home/bnorthan/Brian2014/Projects/deconware2/cube_2.tif",
+			// normalization);
+
+		}
+		catch (Exception ex) {
+
+		}
+
+	}
+
+	/**
+	 * perform one iteration of the algorithm. Sub-classes need to implement this
+	 * function.
+	 */
 	abstract protected void performIteration();
+
+	/**
+	 * TODO: Make this function an op or separate into utility
+	 * 
+	 * @param randomAccessible
+	 * @param position
+	 * @param intensity
+	 */
+	public static <T extends RealType<T>> void drawPoint(
+		final RandomAccessibleInterval<T> randomAccessible, final Point position,
+		final double intensity)
+	{
+		RandomAccess<T> randomAccess = randomAccessible.randomAccess();
+
+		randomAccess.setPosition(position);
+
+		randomAccess.get().setReal(intensity);
+	}
+
+	/**
+	 * TODO: Make this function an op or separate into utility
+	 * 
+	 * @param randomAccessible
+	 * @param start
+	 * @param size
+	 * @param intensity
+	 */
+	public static <T extends RealType<T>> void drawCube(
+		final RandomAccessibleInterval<T> randomAccessible, final Point start,
+		final Point size, final double intensity)
+	{
+		// assume 2-D or 3-D space for now
+		if (start.numDimensions() == 2) {
+			Point position = new Point(2);
+
+			int yStart = start.getIntPosition(1);
+			int xStart = start.getIntPosition(0);
+
+			for (int y = 0; y < size.getIntPosition(1); y++) {
+				position.setPosition(y + yStart, 1);
+				for (int x = 0; x < size.getIntPosition(0); x++) {
+					position.setPosition(x + xStart, 0);
+					drawPoint(randomAccessible, position, intensity);
+				}
+			}
+		}
+		if (start.numDimensions() == 3) {
+			Point position = new Point(3);
+
+			int zStart = start.getIntPosition(2);
+			int yStart = start.getIntPosition(1);
+			int xStart = start.getIntPosition(0);
+
+			for (int z = 0; z < size.getIntPosition(2); z++) {
+				position.setPosition(z + zStart, 2);
+				for (int y = 0; y < size.getIntPosition(1); y++) {
+					position.setPosition(y + yStart, 1);
+					for (int x = 0; x < size.getIntPosition(0); x++) {
+						position.setPosition(x + xStart, 0);
+						drawPoint(randomAccessible, position, intensity);
+					}
+				}
+			}
+		}
+
+	}
 
 	protected RandomAccessibleInterval<O> getRAIExtendedReblurred() {
 		return raiExtendedReblurred;
@@ -164,6 +349,44 @@ public abstract class IterativeFFTFilterRAI<I extends RealType<I>, O extends Rea
 
 	protected RandomAccessibleInterval<O> getRAIExtendedEstimate() {
 		return raiExtendedEstimate;
+	}
+
+	protected ImgFactory<O> getImgFactory() {
+		return imgFactory;
+	}
+
+	public OutOfBoundsFactory<O, RandomAccessibleInterval<O>> getObfOutput() {
+		return obfOutput;
+	}
+
+	public void setObfOutput(
+		OutOfBoundsFactory<O, RandomAccessibleInterval<O>> obfOutput)
+	{
+		this.obfOutput = obfOutput;
+	}
+
+	public Interval getImgConvolutionInterval() {
+		return imgConvolutionInterval;
+	}
+
+	public void setImgConvolutionInterval(Interval imgConvolutionInterval) {
+		this.imgConvolutionInterval = imgConvolutionInterval;
+	}
+
+	public long[] getK() {
+		return k;
+	}
+
+	public long[] getL() {
+		return l;
+	}
+
+	public boolean getNonCirculant() {
+		return nonCirculant;
+	}
+
+	public Img<O> getNormalization() {
+		return normalization;
 	}
 
 }
