5d290aeb3d55b636710ca86b0202cc74ac356c4d
dietzc <dietzc85@googlemail.com>
2015-06-25 22:29:48 +0200

    Add new CreateImgFromImgOp to create Imgs from Imgs

---
+++ src/main/java/net/imagej/ops/create/CreateImgFromImg.java
@@ -0,0 +1,86 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.create;
+
+import net.imagej.ops.Op;
+import net.imagej.ops.OpService;
+import net.imagej.ops.OutputOp;
+import net.imagej.ops.create.CreateOps.CreateNativeImg;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.Type;
+
+import org.scijava.ItemIO;
+import org.scijava.Priority;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Create an {@link Img} from another {@link Img} implementation using its
+ * {@link Type} and {@link ImgFactory}.
+ *
+ * @author Christian Dietz, University of Konstanz.
+ * @param <T>
+ */
+@Plugin(type = Op.class, priority = Priority.HIGH_PRIORITY)
+public class CreateImgFromImg<T extends NativeType<T>> implements
+	CreateNativeImg, OutputOp<Img<T>>
+{
+
+	@Parameter
+	private OpService ops;
+
+	@Parameter(type = ItemIO.OUTPUT)
+	private Img<T> output;
+
+	@Parameter
+	private Img<T> input;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void run() {
+		output =
+			(Img<T>) ops.run(DefaultCreateImg.class, input, input.firstElement()
+				.createVariable(), input.factory());
+	}
+
+	@Override
+	public Img<T> getOutput() {
+		return output;
+	}
+
+	@Override
+	public void setOutput(final Img<T> output) {
+		this.output = output;
+	}
+
+}
