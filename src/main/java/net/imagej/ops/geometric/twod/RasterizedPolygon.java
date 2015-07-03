ea6bd85161d47f346607ba3c4acf3a6919c368bb
DanielSeebacher <danielseebacher@t-online.de>
2015-07-03 10:49:09 +0200

    use faster rasterization algorithm

--- src/main/java/net/imagej/ops/geometric/twod/RasterizedPolygon.java
+++ src/main/java/net/imagej/ops/geometric/twod/RasterizedPolygon.java
@@ -1,8 +1,13 @@
 
 package net.imagej.ops.geometric.twod;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import net.imagej.Extents;
 import net.imagej.Position;
@@ -18,9 +23,8 @@ import net.imglib2.type.logic.BoolType;
 import net.imglib2.util.Intervals;
 
 /**
- * Polygon Rasterizer. FIXME: currently using the worst possible algorithm to
- * rasterize the polygon. for every point from min to max a point in polygon
- * test is made.
+ * Polygon Rasterizer. Uses the algorithm from
+ * http://alienryderflex.com/polygon_fill/.
  * 
  * @author Daniel Seebacher, University of Konstanz.
  */
@@ -28,13 +32,52 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 
 	private Polygon p;
 
+	private final Map<Integer, List<Integer>> scanline;
+
 	public RasterizedPolygon(final Polygon p) {
 		this.p = p;
+
+		this.scanline = createScanLine(p);
+	}
+
+	private Map<Integer, List<Integer>> createScanLine(final Polygon p2) {
+
+		final Map<Integer, List<Integer>> scl =
+			new HashMap<Integer, List<Integer>>();
+
+		for (int y = (int) Math.floor(p2.min(1)); y < Math.ceil(p2.max(1)); y++) {
+			// Build a list of nodes.
+
+			final List<Integer> xValues = new ArrayList<Integer>();
+			int j = p2.vertices().size() - 1;
+			for (int i = 0; i < p2.vertices().size(); i++) {
+
+				final double polyY_i = p2.vertices().get(i).getDoublePosition(1);
+				final double polyX_i = p2.vertices().get(i).getDoublePosition(0);
+				final double polyY_j = p2.vertices().get(j).getDoublePosition(1);
+				final double polyX_j = p2.vertices().get(j).getDoublePosition(0);
+
+				if (polyY_i < y && polyY_j >= y || polyY_j < y && polyY_i >= y) {
+					xValues.add((int) (polyX_i + (y - polyY_i) / (polyY_j - polyY_i) *
+						(polyX_j - polyX_i)));
+				}
+				j = i;
+			}
+
+			// Sort the nodes, via a simple “Bubble” sort.
+			Collections.sort(xValues);
+
+			if (!xValues.isEmpty()) {
+				scl.put(y, xValues);
+			}
+		}
+
+		return scl;
 	}
 
 	@Override
 	public Cursor<BoolType> cursor() {
-		return new RasterizedPolygonCursor(this.p);
+		return new RasterizedPolygonCursor(this.p, this.scanline);
 	}
 
 	@Override
@@ -139,19 +182,20 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 
 	@Override
 	public RandomAccess<BoolType> randomAccess() {
-		return new RasterizedPolygonRandomAcces(this.p);
+		return new RasterizedPolygonRandomAcces(this.p, this.scanline);
 	}
 
 	@Override
 	public RandomAccess<BoolType> randomAccess(final Interval interval) {
-		return new RasterizedPolygonRandomAcces(this.p, interval);
+		return new RasterizedPolygonRandomAcces(interval, this.scanline);
 	}
 
 	private static final class RasterizedPolygonCursor extends
 		AbstractCursor<BoolType>
 	{
 
-		private final Polygon p;
+		private final Interval p;
+		private final Map<Integer, List<Integer>> scl;
 
 		private long[] maxAsLongArray;
 		private long[] minAsLongArray;
@@ -159,13 +203,17 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 		private Point currentPosition;
 		private BoolType currentValue;
 
-		public RasterizedPolygonCursor(final Polygon p) {
+		public RasterizedPolygonCursor(final Interval p,
+			final Map<Integer, List<Integer>> scanline)
+		{
 			super(p.numDimensions());
 
 			this.p = p;
 
 			this.minAsLongArray = Intervals.minAsLongArray(p);
 			this.maxAsLongArray = Intervals.maxAsLongArray(p);
+
+			this.scl = scanline;
 
 			this.currentPosition = null;
 			this.currentValue = null;
@@ -194,7 +242,25 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 				}
 			}
 
-			this.currentValue = new BoolType(this.p.contains(this.currentPosition));
+			final List<Integer> list = this.scl.get(this.currentPosition
+				.getIntPosition(1));
+			if (list == null) {
+				this.currentValue = new BoolType(false);
+				return;
+			}
+
+			final int currentx = this.currentPosition.getIntPosition(0);
+			for (int i = 0; i < list.size(); i += 2) {
+				final int x0 = list.get(i);
+				final int x1 = list.get(i + 1);
+
+				if (currentx >= x0 && currentx <= x1) {
+					this.currentValue = new BoolType(true);
+					return;
+				}
+			}
+
+			this.currentValue = new BoolType(false);
 		}
 
 		@Override
@@ -235,7 +301,7 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 
 		@Override
 		public AbstractCursor<BoolType> copy() {
-			return new RasterizedPolygonCursor(this.p);
+			return new RasterizedPolygonCursor(this.p, this.scl);
 		}
 
 		@Override
@@ -248,22 +314,17 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 		implements RandomAccess<BoolType>
 	{
 
-		private final Polygon p;
+		private final Interval interval;
+		private Map<Integer, List<Integer>> scl;
 
-		public RasterizedPolygonRandomAcces(final Polygon p) {
+		public RasterizedPolygonRandomAcces(final Interval p,
+			final Map<Integer, List<Integer>> scanline)
+		{
 			super(new Extents(Intervals.minAsLongArray(p), Intervals.maxAsLongArray(
 				p)));
 
-			this.p = p;
-		}
-
-		public RasterizedPolygonRandomAcces(final Polygon p,
-			final Interval interval)
-		{
-			super(new Extents(Intervals.minAsLongArray(interval), Intervals
-				.maxAsLongArray(interval)));
-
-			this.p = p;
+			this.interval = p;
+			this.scl = scanline;
 		}
 
 		@Override
@@ -272,12 +333,29 @@ public class RasterizedPolygon implements IterableRegion<BoolType> {
 			final long[] pos = new long[numDimensions()];
 			this.localize(pos);
 
-			return new BoolType(this.p.contains(new Point(pos)));
+			final int currenty = this.getIntPosition(this.getIntPosition(1));
+
+			final List<Integer> list = this.scl.get(currenty);
+			if (list == null) {
+				return new BoolType(false);
+			}
+
+			final int currentx = this.getIntPosition(this.getIntPosition(0));
+			for (int i = 0; i < list.size(); i += 2) {
+				final int x0 = list.get(i);
+				final int x1 = list.get(i + 1);
+
+				if (currentx >= x0 && currentx <= x1) {
+					return new BoolType(true);
+				}
+			}
+
+			return new BoolType(false);
 		}
 
 		@Override
 		public RasterizedPolygonRandomAcces copy() {
-			return new RasterizedPolygonRandomAcces(this.p);
+			return new RasterizedPolygonRandomAcces(this.interval, this.scl);
 		}
 
 		@Override
