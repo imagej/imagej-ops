67061123fd3cac5a31a4b92e806343bc82377101
Curtis Rueden <ctrueden@wisc.edu>
2015-07-07 11:28:03 -0500

    StatsNamespace: clean up and add built-in methods
    Several methods were missing, as were the @OpMethod annotations.

--- src/main/java/net/imagej/ops/stats/StatsNamespace.java
+++ src/main/java/net/imagej/ops/stats/StatsNamespace.java
@@ -33,8 +33,10 @@ package net.imagej.ops.stats;
 import java.util.List;
 
 import net.imagej.ops.AbstractNamespace;
+import net.imagej.ops.OpMethod;
 import net.imagej.ops.Ops;
 import net.imagej.ops.misc.Size;
+import net.imagej.ops.stats.mean.Mean;
 import net.imagej.ops.stats.moment1AboutMean.Moment2AboutMean;
 import net.imagej.ops.stats.sum.Sum;
 import net.imagej.ops.stats.variance.Variance;
@@ -52,10 +54,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- max --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Max.class)
 	public Object max(final Object... args) {
 		return ops().run(Ops.Stats.Max.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.max.MaxRealType.class)
 	public <T extends RealType<T>> T max(final T out, final Iterable<T> in) {
 		@SuppressWarnings("unchecked")
 		final T result =
@@ -65,10 +69,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- mean --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Mean.class)
 	public Object mean(final Object... args) {
 		return ops().run(Ops.Stats.Mean.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.mean.MeanRealType.class)
 	public <I extends RealType<I>, O extends RealType<O>> O mean(final O out,
 		final Iterable<I> in)
 	{
@@ -78,6 +84,7 @@ public class StatsNamespace extends AbstractNamespace {
 		return result;
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.mean.MeanRealType.class)
 	public <I extends RealType<I>, O extends RealType<O>> O mean(final O out,
 		final Iterable<I> in, final Sum<Iterable<I>, O> sumFunc)
 	{
@@ -87,6 +94,7 @@ public class StatsNamespace extends AbstractNamespace {
 		return result;
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.mean.MeanRealType.class)
 	public <I extends RealType<I>, O extends RealType<O>> O mean(final O out,
 		final Iterable<I> in, final Sum<Iterable<I>, O> sumFunc,
 		final Size<Iterable<I>> sizeFunc)
@@ -100,10 +108,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- median --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Median.class)
 	public Object median(final Object... args) {
 		return ops().run(Ops.Stats.Median.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.median.MedianRealType.class)
 	public <T extends RealType<T>> T median(final T out, final Iterable<T> in) {
 		@SuppressWarnings("unchecked")
 		final T result =
@@ -113,10 +123,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- min --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Min.class)
 	public Object min(final Object... args) {
 		return ops().run(Ops.Stats.Min.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.min.MinRealType.class)
 	public <T extends RealType<T>> T min(final T out, final Iterable<T> in) {
 		@SuppressWarnings("unchecked")
 		final T result =
@@ -126,10 +138,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- minMax --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.MinMax.class)
 	public Object minMax(final Object... args) {
 		return ops().run(Ops.Stats.MinMax.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.misc.MinMaxRealType.class)
 	public <T extends RealType<T>> List<T> minMax(final Iterable<T> img) {
 		@SuppressWarnings("unchecked")
 		final List<T> result =
@@ -137,24 +151,65 @@ public class StatsNamespace extends AbstractNamespace {
 		return result;
 	}
 
+	// -- moment1AboutMean --
+
+	@OpMethod(op = net.imagej.ops.stats.moment1AboutMean.Moment1AboutMean.class)
+	public <T extends RealType<T>> DoubleType moment1AboutMean(
+		final DoubleType out, final Iterable<T> in)
+	{
+		final DoubleType result =
+			(DoubleType) ops().run(
+				net.imagej.ops.stats.moment1AboutMean.Moment1AboutMean.class, out, in);
+		return result;
+	}
+
+	@OpMethod(op = net.imagej.ops.stats.moment1AboutMean.Moment1AboutMean.class)
+	public <T extends RealType<T>> DoubleType moment1AboutMean(
+		final DoubleType out, final Iterable<T> in,
+		final Mean<Iterable<T>, DoubleType> mean)
+	{
+		final DoubleType result =
+			(DoubleType) ops().run(
+				net.imagej.ops.stats.moment1AboutMean.Moment1AboutMean.class, out, in,
+				mean);
+		return result;
+	}
+
+	@OpMethod(ops = {
+		net.imagej.ops.stats.moment1AboutMean.Moment1AboutMean.class,
+		net.imagej.ops.stats.moment1AboutMean.Moment2AboutMean.class })
+	public <T extends RealType<T>> DoubleType moment1AboutMean(
+		final DoubleType out, final Iterable<T> in,
+		final Mean<Iterable<T>, DoubleType> mean, final Size<Iterable<T>> size)
+	{
+		final DoubleType result =
+			(DoubleType) ops().run(net.imagej.ops.Ops.Stats.Moment1AboutMean.class,
+				out, in, mean, size);
+		return result;
+	}
+
 	// -- quantile --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Quantile.class)
 	public Object quantile(final Object... args) {
 		return ops().run(Ops.Stats.Quantile.NAME, args);
 	}
 
 	// -- size --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Size.class)
 	public Object size(final Object... args) {
 		return ops().run(Ops.Stats.Size.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.misc.SizeIterableInterval.class)
 	public LongType size(final LongType out, final IterableInterval<?> in) {
 		final LongType result =
 			(LongType) ops().run(net.imagej.ops.misc.SizeIterableInterval.class, out, in);
 		return result;
 	}
 
+	@OpMethod(op = net.imagej.ops.misc.SizeIterable.class)
 	public LongType size(final LongType out, final Iterable<?> in) {
 		final LongType result =
 			(LongType) ops().run(net.imagej.ops.misc.SizeIterable.class, out, in);
@@ -163,10 +218,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- stdDev --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.StdDeviation.class)
 	public Object stdDev(final Object... args) {
 		return ops().run(Ops.Stats.StdDeviation.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.stdDev.StdDevRealTypeDirect.class)
 	public <T extends RealType<T>> T stdDev(final T out, final Iterable<T> in) {
 		@SuppressWarnings("unchecked")
 		final T result =
@@ -174,6 +231,7 @@ public class StatsNamespace extends AbstractNamespace {
 		return result;
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.stdDev.StdDevRealType.class)
 	public <T extends RealType<T>> DoubleType stdDev(final DoubleType out,
 		final Iterable<T> in)
 	{
@@ -182,6 +240,7 @@ public class StatsNamespace extends AbstractNamespace {
 		return result;
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.stdDev.StdDevRealType.class)
 	public <T extends RealType<T>> DoubleType stdDev(final DoubleType out,
 		final Iterable<T> in, final Variance<T, DoubleType> variance)
 	{
@@ -193,10 +252,12 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- sum --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Sum.class)
 	public Object sum(final Object... args) {
 		return ops().run(Ops.Stats.Sum.NAME, args);
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.sum.SumRealType.class)
 	public <T extends RealType<T>, V extends RealType<V>> V sum(final V out,
 		final Iterable<T> in)
 	{
@@ -208,19 +269,22 @@ public class StatsNamespace extends AbstractNamespace {
 
 	// -- variance --
 
+	@OpMethod(op = net.imagej.ops.Ops.Stats.Variance.class)
 	public Object variance(final Object... args) {
 		return ops().run(Ops.Stats.Variance.NAME, args);
 	}
 
+	@OpMethod(ops = { net.imagej.ops.stats.variance.VarianceRealType.class,
+		net.imagej.ops.stats.variance.VarianceRealTypeDirect.class })
 	public <T extends RealType<T>> DoubleType variance(final DoubleType out,
 		final Iterable<T> in)
 	{
 		final DoubleType result =
-			(DoubleType) ops().run(net.imagej.ops.stats.variance.VarianceRealTypeDirect.class,
-				out, in);
+			(DoubleType) ops().run(net.imagej.ops.Ops.Stats.Variance.class, out, in);
 		return result;
 	}
 
+	@OpMethod(op = net.imagej.ops.stats.variance.VarianceRealType.class)
 	public <T extends RealType<T>> DoubleType variance(final DoubleType out,
 		final Iterable<T> in, final Moment2AboutMean<T> moment2)
 	{
