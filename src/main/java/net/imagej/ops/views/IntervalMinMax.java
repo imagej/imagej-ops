ed012d883e762cf51a64b8bc86291b76e58fa101
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-29 10:53:10 +0200

    Rename ViewMinMax to IntervalMinMax.

--- src/main/java/net/imagej/ops/views/IntervalMinMax.java
+++ src/main/java/net/imagej/ops/views/IntervalMinMax.java
@@ -4,22 +4,22 @@ import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import net.imagej.ops.view.ViewOps;
-import net.imagej.ops.view.ViewOps.View;
+import net.imagej.ops.AbstractFunctionOp;
+import net.imagej.ops.Ops;
 import net.imglib2.RandomAccessible;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
-@Plugin(type = ViewOps.View.class, name = ViewOps.View.NAME)
-public class ViewMinMax< T > extends AbstractView<RandomAccessible<T>, IntervalView< T >> implements
-		View<RandomAccessible<T>, IntervalView< T >> {
+@Plugin(type = Ops.View.Interval.class, name = Ops.View.Interval.NAME)
+public class IntervalMinMax<T> extends AbstractFunctionOp<RandomAccessible<T>, IntervalView<T>>
+		implements Ops.View.Interval {
 
 	@Parameter(type = ItemIO.INPUT)
 	private long[] min;
-	
+
 	@Parameter(type = ItemIO.INPUT)
 	private long[] max;
-	
+
 	@Override
 	public IntervalView<T> compute(RandomAccessible<T> input) {
 		return Views.interval(input, min, max);
