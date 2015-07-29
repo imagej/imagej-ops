e1b9e239b64c036806693ab9fca72442519c68cc
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-07-29 10:53:30 +0200

    Rename DefaultView to DefaultInterval.

---
+++ src/main/java/net/imagej/ops/views/DefaultInterval.java
@@ -0,0 +1,26 @@
+package net.imagej.ops.views;
+
+import org.scijava.ItemIO;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.AbstractFunctionOp;
+import net.imagej.ops.Ops;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccessible;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+
+@Plugin(type = Ops.View.Interval.class, name = Ops.View.Interval.NAME)
+public class DefaultInterval<T> extends AbstractFunctionOp<RandomAccessible<T>, IntervalView<T>>
+		implements Ops.View.Interval {
+
+	@Parameter(type = ItemIO.INPUT)
+	private Interval interval;
+
+	@Override
+	public IntervalView<T> compute(RandomAccessible<T> input) {
+		return Views.interval(input, interval);
+	}
+
+}
