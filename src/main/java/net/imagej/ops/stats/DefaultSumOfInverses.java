994a38832dbc0a9262e131ffef843970c1b9547f
Daniel Seebacher <danielseebacher@t-online.de>
2015-08-07 12:22:13 +0200

    add license headers and javadoc.
    also changed some <T extends RealType<T>> to <I extends RealType<I>> for better consistency.

--- src/main/java/net/imagej/ops/stats/DefaultSumOfInverses.java
+++ src/main/java/net/imagej/ops/stats/DefaultSumOfInverses.java
@@ -1,21 +1,59 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
 
 package net.imagej.ops.stats;
 
 import org.scijava.plugin.Plugin;
 
+import net.imagej.ops.Op;
 import net.imagej.ops.Ops.Stats.SumOfInverses;
 import net.imglib2.type.numeric.RealType;
 
+/**
+ * {@link Op} to calculate the {@link SumOfInverses}
+ * 
+ * @author Daniel Seebacher, University of Konstanz.
+ * @author Christian Dietz, University of Konstanz.
+ * @param <I> input type
+ * @param <O> output type
+ */
 @Plugin(type = StatOp.class, name = SumOfInverses.NAME,
 	label = "Statistics: Sum Of Inverses")
-public class DefaultSumOfInverses<T extends RealType<T>, O extends RealType<O>> extends
-	AbstractStatOp<Iterable<T>, O>implements SumOfInverses
+public class DefaultSumOfInverses<I extends RealType<I>, O extends RealType<O>> extends
+	AbstractStatOp<Iterable<I>, O>implements SumOfInverses
 {
 
 	@Override
-	public void compute(final Iterable<T> input, final O output) {
+	public void compute(final Iterable<I> input, final O output) {
 		double res = 0.0;
-		for (final T in : input) {
+		for (final I in : input) {
 			res += (1.0d / in.getRealDouble());
 		}
 		output.setReal(res);
