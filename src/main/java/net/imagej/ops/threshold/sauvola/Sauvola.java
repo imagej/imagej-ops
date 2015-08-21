47372b66e299a30007b01774eb1d43f6d9c4962b
Stefan Helfrich <s.helfrich@fz-juelich.de>
2015-08-21 10:19:30 +0200

    Cleaned up code and added (authorship) information

--- src/main/java/net/imagej/ops/threshold/sauvola/Sauvola.java
+++ src/main/java/net/imagej/ops/threshold/sauvola/Sauvola.java
@@ -12,14 +12,13 @@ import net.imglib2.type.numeric.real.DoubleType;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import ij.IJ;
-import ij.ImagePlus;
-import ij.plugin.filter.RankFilters;
-import ij.process.ImageConverter;
-import ij.process.ImageProcessor;
-
 /**
- * TODO Documentation
+ * This is a modification of Niblack's thresholding method.
+ * Sauvola J. and Pietaksinen M. (2000) "Adaptive Document Image Binarization"
+ * Pattern Recognition, 33(2): 225-236
+ * http://www.ee.oulu.fi/mvg/publications/show_pdf.php?ID=24
+ * Ported to ImageJ plugin from E Celebi's fourier_0.8 routines.
+ * Original ImageJ1 implementation by Gabriel Landini.
  * 
  * @author Stefan Helfrich <s.helfrich@fz-juelich.de>
  */
@@ -46,12 +45,7 @@ public class Sauvola<T extends RealType<T>> extends LocalThresholdMethod<T>
 	@Override
 	public void compute(final Pair<T> input, final BitType output) {
 		// Sauvola recommends K_VALUE = 0.5 and R_VALUE = 128.
-		// This is a modification of Niblack's thresholding method.
-		// Sauvola J. and Pietaksinen M. (2000) "Adaptive Document Image Binarization"
-		// Pattern Recognition, 33(2): 225-236
-		// http://www.ee.oulu.fi/mvg/publications/show_pdf.php?ID=24
-		// Ported to ImageJ plugin from E Celebi's fourier_0.8 routines
-		// This version uses a circular local window, instead of a rectagular one		
+
 		if (mean == null) {
 			mean = ops.op(MeanOp.class, DoubleType.class, input.neighborhood);
 		}
