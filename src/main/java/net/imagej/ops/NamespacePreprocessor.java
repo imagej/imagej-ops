79e948e6f378e35e3c16073c209f6bb30fcba9d2
Mark Hiner <hinerm@gmail.com>
2015-09-10 15:54:53 -0500

    Create NamespacePreprocessor
    This preprocessor plugin uses the NamespaceService to populate
    Namespace inputs of modules. This allows, for example, scripts to use
    @NamespaceType directly now!
    
    Closes #196

---
+++ src/main/java/net/imagej/ops/NamespacePreprocessor.java
@@ -0,0 +1,78 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops;
+
+import org.scijava.Priority;
+import org.scijava.module.Module;
+import org.scijava.module.ModuleItem;
+import org.scijava.module.process.AbstractPreprocessorPlugin;
+import org.scijava.module.process.PreprocessorPlugin;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * {@link PreprocessorPlugin} implementation that looks up requested
+ * {@link Namespace} implementations, obtaining their singleton instance handled
+ * by the {@link NamespaceService}.
+ *
+ * @author Mark Hiner
+ */
+@Plugin(type = PreprocessorPlugin.class, priority = Priority.HIGH_PRIORITY)
+public class NamespacePreprocessor extends AbstractPreprocessorPlugin {
+
+	@Parameter(required = false)
+	private NamespaceService nsService;
+
+	@Override
+	public void process(final Module module) {
+		for (final ModuleItem<?> input : module.getInfo().inputs()) {
+			assignNamespace(module, input);
+		}
+	}
+
+	// -- Helper methods --
+
+	@SuppressWarnings("unchecked")
+	private <T> void assignNamespace(final Module module,
+		final ModuleItem<T> item)
+	{
+		if (module.isResolved(item.getName())) return;
+		T defaultValue = null;
+		
+		if (Namespace.class.isAssignableFrom(item.getType())) {
+			defaultValue = (T) nsService.getInstance((Class<? extends Namespace>)item.getType());
+		}
+		if (defaultValue == null) return;
+
+		item.setValue(module, defaultValue);
+		module.setResolved(item.getName(), true);
+	}
+}
