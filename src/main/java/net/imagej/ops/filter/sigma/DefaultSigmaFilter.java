b037d6697be178f747e64362466431f790c60040
Curtis Rueden <ctrueden@wisc.edu>
2015-09-11 12:26:02 -0500

    DefaultSigmaFilter: inject the OpEnvironment
    If you make a new Op directly (i.e., without using ops().op(...)),
    then you need to inject the environment manually.

--- src/main/java/net/imagej/ops/filter/sigma/DefaultSigmaFilter.java
+++ src/main/java/net/imagej/ops/filter/sigma/DefaultSigmaFilter.java
@@ -37,7 +37,8 @@ public class DefaultSigmaFilter<T extends RealType<T>> extends
 	@Override
 	protected CenterAwareComputerOp<T, T> getComputer(Class<?> inClass,
 			Class<?> outClass) {
-		return new AbstractCenterAwareComputerOp<T, T>() {
+		final AbstractCenterAwareComputerOp<T, T> op =
+			new AbstractCenterAwareComputerOp<T, T>() {
 
 			private ComputerOp<Iterable<T>, DoubleType> variance;
 
@@ -83,6 +84,8 @@ public class DefaultSigmaFilter<T extends RealType<T>> extends
 			}
 
 		};
+		op.setEnvironment(ops());
+		return op;
 	}
 
 	@Override
