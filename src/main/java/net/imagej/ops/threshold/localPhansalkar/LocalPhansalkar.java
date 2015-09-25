1a1e3d63fc9dc722b2b220ac93e38c0acd411b7a
Stefan Helfrich <s.helfrich@fz-juelich.de>
2015-09-25 08:18:57 +0200

    Added some clarifying documentation

--- src/main/java/net/imagej/ops/threshold/localPhansalkar/LocalPhansalkar.java
+++ src/main/java/net/imagej/ops/threshold/localPhansalkar/LocalPhansalkar.java
@@ -5,7 +5,6 @@ import net.imagej.ops.OpService;
 import net.imagej.ops.Ops;
 import net.imagej.ops.Ops.Stats.Mean;
 import net.imagej.ops.Ops.Stats.StdDev;
-import net.imagej.ops.stats.DefaultMean;
 import net.imagej.ops.threshold.LocalThresholdMethod;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
@@ -19,16 +18,20 @@ import org.scijava.plugin.Plugin;
  * This is a modification of Sauvola's thresholding method to deal with low
  * contrast images.
  * 
- * Phansalskar N. et al. Adaptive local thresholding for detection of nuclei in
- * diversity stained cytology images. International Conference on Communications
- * and Signal Processing (ICCSP), 2011, // 218 - 220.
- * 
- * In this method, the threshold t = mean*(1+p*exp(-q*mean)+k*((stdev/r)-1))
+ * In this algorithm the threshold is computed as t =
+ * mean*(1+p*exp(-q*mean)+k*((stdev/r)-1)) for an image that is normalized to
+ * [0, 1].
  * 
  * Phansalkar recommends k = 0.25, r = 0.5, p = 2 and q = 10. In the current
- * implementation, the values of p and q are fixed.
+ * implementation, the values of p and q are fixed but can be implemented as
+ * additional parameters.
  * 
- * Implemented from Phansalkar's paper description by G. Landini.
+ * Originally implemented from Phansalkar's paper description by G. Landini
+ * (http://fiji.sc/Auto_Local_Threshold#Phansalkar).
+ * 
+ * Phansalskar N. et al. Adaptive local thresholding for detection of nuclei in
+ * diversity stained cytology images. International Conference on Communications
+ * and Signal Processing (ICCSP), 2011, 218 - 220.
  * 
  * @author Stefan Helfrich <s.helfrich@fz-juelich.de>
  */
