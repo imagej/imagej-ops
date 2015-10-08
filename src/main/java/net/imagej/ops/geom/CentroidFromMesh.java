2e793527dbd72a5605dc2955005259cf9ffe5f2c
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2015-10-08 15:26:07 +0200

    Add CentroidFromMesh.

---
+++ src/main/java/net/imagej/ops/geom/CentroidFromMesh.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2015 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.geom;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+import org.scijava.plugin.Plugin;
+
+import net.imagej.ops.AbstractFunctionOp;
+import net.imagej.ops.Ops.Geometric;
+import net.imagej.ops.Ops.Geometric.Centroid;
+import net.imagej.ops.geom.helper.Mesh;
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPoint;
+
+/**
+ * Generic implementation of {@link Centroid}.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz.
+ */
+@Plugin(type = GeometricOp.class, label = "Geometric: Centroid", name = Geometric.Centroid.NAME)
+public class CentroidFromMesh
+		extends
+			AbstractFunctionOp<Mesh, RealLocalizable>
+		implements
+			Centroid {
+
+	@Override
+	public RealLocalizable compute(Mesh input) {
+		LinkedHashSet<RealLocalizable> points = input.getPoints();
+		Iterator<RealLocalizable> it = points.iterator();
+		double x, y, z = y = x = 0;
+		while (it.hasNext()) {
+			RealLocalizable next = it.next();
+			x += next.getDoublePosition(0);
+			y += next.getDoublePosition(1);
+			z += next.getDoublePosition(2);
+		}
+
+		x /= points.size();
+		y /= points.size();
+		z /= points.size();
+
+		return new RealPoint(x, y, z);
+	}
+
+}
