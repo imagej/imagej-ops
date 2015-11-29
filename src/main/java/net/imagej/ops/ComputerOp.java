ac4ad848d612399fcb54cef280e621cb4cb91ac1
Curtis Rueden <ctrueden@wisc.edu>
2015-11-29 14:55:16 -0600

    ComputerOp: implement some methods

--- src/main/java/net/imagej/ops/ComputerOp.java
+++ src/main/java/net/imagej/ops/ComputerOp.java
@@ -53,7 +53,21 @@ public interface ComputerOp<I, O> extends SpecialOp<I, O> {
 	 */
 	void compute(I input, O output);
 
+	// -- Threadable methods --
+
 	@Override
-	ComputerOp<I, O> getIndependentInstance();
+	default ComputerOp<I, O> getIndependentInstance() {
+		// NB: We assume the op instance is thread-safe by default.
+		// Individual implementations can override this assumption if they
+		// have state (such as buffers) that cannot be shared across threads.
+		return this;
+	}
+
+	// -- Runnable methods --
+
+	@Override
+	default void run() {
+		compute(in(), out());
+	}
 
 }
