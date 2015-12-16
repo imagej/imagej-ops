5c969e6e439bb244a639d5df68420823586a4917
djniles <dave.j.niles@gmail.com>
2015-12-16 10:44:10 -0600

    Fix sign in calculation of HuMoment2

--- src/main/java/net/imagej/ops/imagemoments/hu/DefaultHuMoment2.java
+++ src/main/java/net/imagej/ops/imagemoments/hu/DefaultHuMoment2.java
@@ -76,6 +76,6 @@ public class DefaultHuMoment2<I extends RealType<I>, O extends RealType<O>>
 		double n20 = normalizedCentralMoment20Func.compute1(input).getRealDouble();
 		double n02 = normalizedCentralMoment02Func.compute1(input).getRealDouble();
 
-		output.setReal(Math.pow(n20 - n02, 2) - 4 * (Math.pow(n11, 2)));
+		output.setReal(Math.pow(n20 - n02, 2) + 4 * (Math.pow(n11, 2)));
 	}
 }
