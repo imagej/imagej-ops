cdbb325ca2f352dcbbe32c9664687cff49505ff5
Brian Northan <bnorthan@gmail.com>
2016-01-11 19:37:55 -0500

    DivideHandleZero: Make binary

--- src/main/java/net/imagej/ops/math/divide/DivideHandleZero.java
+++ src/main/java/net/imagej/ops/math/divide/DivideHandleZero.java
@@ -2,65 +2,56 @@
 package net.imagej.ops.math.divide;
 
 import org.scijava.Priority;
-import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 import net.imagej.ops.Contingent;
 import net.imagej.ops.Ops;
-import net.imagej.ops.special.AbstractUnaryHybridOp;
+import net.imagej.ops.special.AbstractBinaryComputerOp;
 import net.imglib2.Cursor;
 import net.imglib2.IterableInterval;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.util.Intervals;
 
 @Plugin(type = Ops.Math.Divide.class, priority = Priority.LOW_PRIORITY)
 public class DivideHandleZero<T extends RealType<T>> extends
-	AbstractUnaryHybridOp<IterableInterval<T>, IterableInterval<T>> implements
-	Ops.Math.Divide, Contingent
+	AbstractBinaryComputerOp<IterableInterval<T>, IterableInterval<T>, IterableInterval<T>>
+	implements Ops.Math.Divide, Contingent
 {
-
-	private boolean inPlace;
 
 	// TODO: extend common abstract base class which implements Contingent
 	// for dimensionality checking.
 	// TODO: code generate this and all add ops to generalize them to other
 	// operators.
 
-	@Parameter
-	private IterableInterval<T> ii;
-
 	@Override
 	public boolean conforms() {
-		if (!Intervals.equalDimensions(in(), ii)) return false;
-		if (out() == null) return true;
-		return Intervals.equalDimensions(ii, out());
+		return true;
+		/*	if (!Intervals.equalDimensions(in1(), in2())) return false;
+			if (!in1().iterationOrder().equals(in2().iterationOrder())) return false;
+			if (out() == null) return true;
+			return Intervals.equalDimensions(in1(), out()) && in1().iterationOrder()
+				.equals(out().iterationOrder());*/
 	}
 
 	@Override
-	public IterableInterval<T> createOutput(final IterableInterval<T> input) {
-		inPlace = true;
-		return ops().create().img(input, input.firstElement(), null);
-	}
-
-	@Override
-	public void compute1(final IterableInterval<T> input,
-		final IterableInterval<T> output)
+	public void compute2(final IterableInterval<T> input1,
+		final IterableInterval<T> input2, final IterableInterval<T> output)
 	{
-		final Cursor<T> cursor = ii.cursor();
-		final Cursor<T> cursorI = input.cursor();
+		final Cursor<T> cursorI1 = input1.cursor();
+		final Cursor<T> cursorI2 = input2.cursor();
 		final Cursor<T> cursorO = output.cursor();
 
-		final T tmp = input.firstElement().copy();
+		final T tmp = input1.firstElement().copy();
 
-		while (cursor.hasNext()) {
-			cursor.fwd();
-			cursorI.fwd();
+		while (cursorI1.hasNext()) {
+			cursorI1.fwd();
+			cursorI2.fwd();
+			cursorO.fwd();
 
-			if (!inPlace) cursorO.fwd();
+			// if (!inPlace) cursorO.fwd();
 
-			if (cursor.get().getRealFloat() > 0) {
-				tmp.set(cursorI.get());
-				tmp.div(cursor.get());
+			if (cursorI2.get().getRealFloat() > 0) {
+				tmp.set(cursorI1.get());
+				tmp.div(cursorI2.get());
 				cursorO.get().set(tmp);
 			}
 			else {
