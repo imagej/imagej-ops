9e7439cbfe4c2c289f719e0aeecfea56805e7fcd
Stefan Helfrich <stefan.helfrich@googlemail.com>
2016-01-21 18:28:50 +0100

    Generalize input shape to RectangleShape
    With the previous implementation, LocalMeanRAIIntegral was never matched
    because the parameter was too specific. Now, it is possible and the 
    matching is governed by the priorities of implementations.

--- src/main/java/net/imagej/ops/threshold/localMean/LocalMeanRAIIntegral.java
+++ src/main/java/net/imagej/ops/threshold/localMean/LocalMeanRAIIntegral.java
@@ -39,6 +39,7 @@ import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.integral.IntegralImg;
 import net.imglib2.algorithm.neighborhood.Neighborhood;
+import net.imglib2.algorithm.neighborhood.RectangleShape;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.RealDoubleConverter;
 import net.imglib2.outofbounds.OutOfBoundsFactory;
@@ -58,48 +59,54 @@ import org.scijava.plugin.Plugin;
  * @author Stefan Helfrich (University of Konstanz)
  */
 @Plugin(type = Ops.Threshold.LocalMean.class, priority = Priority.HIGH_PRIORITY)
-public class LocalMeanRAIIntegral<T extends RealType<T> & NativeType<T>>
-	extends AbstractUnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<BitType>>
+public class LocalMeanRAIIntegral<T extends RealType<T> & NativeType<T>> extends
+	AbstractUnaryComputerOp<RandomAccessibleInterval<T>, RandomAccessibleInterval<BitType>>
 	implements Ops.Threshold.LocalMean
 {
-	
+
 	@Parameter
-	private IntegralRectangleShape shape;
+	private RectangleShape shape;
 
 	@Parameter(required = false)
 	private OutOfBoundsFactory<T, RandomAccessibleInterval<T>> outOfBounds;
-	
+
 	@Parameter(required = false)
 	private IntegralImg<T, DoubleType> integralImg;
-	
+
 	@Parameter
 	private double c;
-	
+
 	private IntegralSum<DoubleType> integralSum;
-	
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize() {
-		// FIXME Init integralMean correctly
-		integralSum = ops().op(IntegralSum.class, DoubleType.class, Views.iterable(in()));	
+		integralSum = ops().op(IntegralSum.class, DoubleType.class, Views.iterable(
+			in()));
+
+		if (!(shape instanceof IntegralRectangleShape)) {
+			shape = new IntegralRectangleShape(shape.getSpan() + 1, false);
+		}
 	}
 
 	@Override
-	public void compute1(final RandomAccessibleInterval<T> input, final RandomAccessibleInterval<BitType> output)
+	public void compute1(final RandomAccessibleInterval<T> input,
+		final RandomAccessibleInterval<BitType> output)
 	{
 		// Create IntegralImg from input
-		integralImg = new IntegralImg<>(input, new DoubleType(), new RealDoubleConverter<T>());    
-		
-		// integralImg will be larger by one pixel in each dimension than input due to the computation of the integral image
+		integralImg = new IntegralImg<>(input, new DoubleType(),
+			new RealDoubleConverter<T>());
+
+		// integralImg will be larger by one pixel in each dimension than input due
+		// to the computation of the integral image
 		RandomAccessibleInterval<DoubleType> img = null;
-		if ( integralImg.process() )
-		{
+		if (integralImg.process()) {
 			img = integralImg.getResult();
 		}
-		
+
 		// Remove 0s from integralImg by shifting its interval by +1
-		long[] min = new long[input.numDimensions()];
-		long[] max = new long[input.numDimensions()];
+		final long[] min = new long[input.numDimensions()];
+		final long[] max = new long[input.numDimensions()];
 
 		for (int d = 0; d < input.numDimensions(); ++d) {
 			min[d] = input.min(d) + 1;
@@ -107,87 +114,93 @@ public class LocalMeanRAIIntegral<T extends RealType<T> & NativeType<T>>
 		}
 
 		// Define the Interval on the infinite random accessibles
-		FinalInterval interval = new FinalInterval(min, max);
+		final FinalInterval interval = new FinalInterval(min, max);
 
-		RandomAccessibleInterval<DoubleType> extendedImg = Views.offsetInterval(
-			Views.extendBorder(img), interval);
+		final RandomAccessibleInterval<DoubleType> extendedImg = Views
+			.offsetInterval(Views.extendBorder(img), interval);
 
 		// Random access for input and output
-		RandomAccess<BitType> outputRandomAccess = output.randomAccess();
-		RandomAccess<T> inputRandomAccess = input.randomAccess();
-    
-    shape = new IntegralRectangleShape(shape.getSpan()+1, false);
-		Cursor<Neighborhood<DoubleType>> cursor = shape.neighborhoodsIntegral(extendedImg).cursor();
-		
+		final RandomAccess<BitType> outputRandomAccess = output.randomAccess();
+		final RandomAccess<T> inputRandomAccess = input.randomAccess();
+
+		// Cast is safe due to the initialization of the Op
+		final Cursor<Neighborhood<DoubleType>> cursor =
+			((IntegralRectangleShape) shape).neighborhoodsIntegral(extendedImg)
+				.cursor();
+
 		// Iterate neighborhoods
-		while ( cursor.hasNext() )
-		{
-			Neighborhood<DoubleType> neighborhood = cursor.next();
-			
-			DoubleType sum = new DoubleType();
+		while (cursor.hasNext()) {
+			final Neighborhood<DoubleType> neighborhood = cursor.next();
+
+			final DoubleType sum = new DoubleType();
 			integralSum.compute1(neighborhood, sum);
-			
-			long[] neighborhoodPosition = new long[ neighborhood.numDimensions() ];
+
+			final long[] neighborhoodPosition = new long[neighborhood
+				.numDimensions()];
 			neighborhood.localize(neighborhoodPosition);
-			
+
 			// Absolute difference between minima
-			long[] neighborhoodMinimum = new long[ neighborhood.numDimensions() ];
-			neighborhood.min( neighborhoodMinimum );
+			final long[] neighborhoodMinimum = new long[neighborhood.numDimensions()];
+			neighborhood.min(neighborhoodMinimum);
 			neighborhoodMinimum[0] = neighborhoodMinimum[0] - 1;
 			neighborhoodMinimum[1] = neighborhoodMinimum[1] - 1;
-			
-			long[] neighborhoodMaximum = new long[ neighborhood.numDimensions() ];
-			neighborhood.max( neighborhoodMaximum );
+
+			final long[] neighborhoodMaximum = new long[neighborhood.numDimensions()];
+			neighborhood.max(neighborhoodMaximum);
 			neighborhoodMaximum[0] = neighborhoodMaximum[0] + 1;
 			neighborhoodMaximum[1] = neighborhoodMaximum[1] + 1;
-			
-			long[] inputMinimum = new long[ extendedImg.numDimensions() ];
-			extendedImg.min( inputMinimum );
-			
-			long[] inputMaximum = new long[ extendedImg.numDimensions() ];
-			extendedImg.max( inputMaximum );
-			
+
+			final long[] inputMinimum = new long[extendedImg.numDimensions()];
+			extendedImg.min(inputMinimum);
+
+			final long[] inputMaximum = new long[extendedImg.numDimensions()];
+			extendedImg.max(inputMaximum);
+
 			int area = 1;
-			for ( int d = 0; d < input.numDimensions(); d++ )
-			{
-				if (neighborhoodMinimum[ d ] <= inputMinimum[ d ] && neighborhoodMaximum[ d ] <= inputMaximum[ d ])
+			for (int d = 0; d < input.numDimensions(); d++) {
+				if (neighborhoodMinimum[d] <= inputMinimum[d] &&
+					neighborhoodMaximum[d] <= inputMaximum[d])
 				{
-					area *= Math.abs((neighborhoodMaximum[ d ] + 1) - inputMinimum[ d ]);
+					area *= Math.abs((neighborhoodMaximum[d] + 1) - inputMinimum[d]);
 					continue;
 				}
-				
-				if (neighborhoodMinimum[ d ] >= inputMinimum[ d ] && neighborhoodMaximum[ d ] <= inputMaximum[ d ])
+
+				if (neighborhoodMinimum[d] >= inputMinimum[d] &&
+					neighborhoodMaximum[d] <= inputMaximum[d])
 				{
-					area *= Math.abs((neighborhoodMaximum[ d ] + 1) - neighborhoodMinimum[ d ]);
+					area *= Math.abs((neighborhoodMaximum[d] + 1) -
+						neighborhoodMinimum[d]);
 					continue;
 				}
-				
-				if (neighborhoodMinimum[ d ] >= inputMinimum[ d ] && neighborhoodMaximum[ d ] >= inputMaximum[ d ])
+
+				if (neighborhoodMinimum[d] >= inputMinimum[d] &&
+					neighborhoodMaximum[d] >= inputMaximum[d])
 				{
-					area *= Math.abs((inputMaximum[ d ] + 1) - neighborhoodMinimum[ d ]);
+					area *= Math.abs((inputMaximum[d] + 1) - neighborhoodMinimum[d]);
 					continue;
 				}
-				
-				// FIXME Missing case that implies that the RectangleShape is bigger than the image itself
+
+				// FIXME Missing case that implies that the RectangleShape is bigger
+				// than the image itself
 			}
-			
+
 			// Compute mean by dividing the sum divided by the number of elements
 			sum.div(new DoubleType(area));
-			
+
 			// Subtract the contrast
 			sum.sub(new DoubleType(c));
-			
+
 			// Set value
 			inputRandomAccess.setPosition(neighborhoodPosition);
-			T inputPixel = inputRandomAccess.get();
+			final T inputPixel = inputRandomAccess.get();
 
-			Converter<T, DoubleType> conv = new RealDoubleConverter<>();
-			DoubleType inputPixelAsDoubleType = new DoubleType();
+			final Converter<T, DoubleType> conv = new RealDoubleConverter<>();
+			final DoubleType inputPixelAsDoubleType = new DoubleType();
 			conv.convert(inputPixel, inputPixelAsDoubleType);
-			
+
 			outputRandomAccess.setPosition(neighborhood);
 			outputRandomAccess.get().set(inputPixelAsDoubleType.compareTo(sum) > 0);
 		}
 	}
-	
+
 }
