db18e847a6711b53018d212daa33c0ba5011ae63
Stefan Helfrich <stefan.helfrich@googlemail.com>
2016-01-26 14:34:17 +0100

    Test consistency of results for LocalMeans

--- src/test/java/net/imagej/ops/threshold/apply/LocalThresholdTest.java
+++ src/test/java/net/imagej/ops/threshold/apply/LocalThresholdTest.java
@@ -32,6 +32,8 @@ package net.imagej.ops.threshold.apply;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Random;
+
 import net.imagej.ops.AbstractOpTest;
 import net.imagej.ops.threshold.LocalThresholdMethod;
 import net.imagej.ops.threshold.ThresholdNamespace;
@@ -45,12 +47,20 @@ import net.imagej.ops.threshold.localNiblack.LocalNiblackThreshold;
 import net.imagej.ops.threshold.localPhansalkar.LocalPhansalkarThreshold;
 import net.imagej.ops.threshold.localSauvola.LocalSauvolaThreshold;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
 import net.imglib2.algorithm.neighborhood.RectangleShape;
+import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.ByteArray;
 import net.imglib2.outofbounds.OutOfBoundsMirrorFactory;
 import net.imglib2.outofbounds.OutOfBoundsMirrorFactory.Boundary;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.ValuePair;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -188,6 +198,50 @@ public class LocalThresholdTest extends AbstractOpTest {
 	}
 
 	/**
+	 * @see LocalMeanRAIIntegral
+	 * @see LocalMeanThreshold
+	 */
+	@Test
+	public void testLocalMeanResultsConsistency() {
+		Img<BitType> out2 = null;
+		Img<BitType> out3 = null;
+		try {
+			out2 = in.factory().imgFactory(new BitType()).create(in, new BitType());
+			out3 = in.factory().imgFactory(new BitType()).create(in, new BitType());
+		}
+		catch (IncompatibleTypeException exc) {
+			exc.printStackTrace();
+		}
+		
+		// Default implementation
+		ops.threshold().apply(
+			out2,
+			in,
+			ops.op(LocalMeanThreshold.class, BitType.class,
+				new ValuePair<ByteType, Iterable<ByteType>>(null, in), 0.0),
+			new RectangleShape(1, false),
+			new OutOfBoundsMirrorFactory<ByteType, Img<ByteType>>(Boundary.SINGLE));
+		
+		// Integral image-based implementation
+		ops.run(LocalMeanRAIIntegral.class,
+			out3,
+			in,
+			new RectangleShape(1, false),
+			new OutOfBoundsMirrorFactory<ByteType, Img<ByteType>>(Boundary.SINGLE),
+			0.0);
+		
+		// Test for pixel-wise equality of the results
+		Cursor<BitType> cursorOut2 = out2.cursor();
+		Cursor<BitType> cursorOut3 = out3.cursor();
+		while	(cursorOut2.hasNext()  && cursorOut3.hasNext()) {
+			BitType valueOut2 = cursorOut2.next();
+			BitType valueOut3 = cursorOut3.next();
+			
+			assertEquals(valueOut2, valueOut3);
+		}
+	}
+
+	/**
 	 * @see LocalMedianThreshold
 	 */
 	@Test
@@ -244,6 +298,92 @@ public class LocalThresholdTest extends AbstractOpTest {
 			0.0, 0.0);
 
 		assertEquals(out.firstElement().get(), false);
+	}
+
+	public ArrayImg<ByteType, ByteArray> generateOnesTestImg(final boolean fill,
+		final long... dims)
+	{
+		final byte[] array =
+			new byte[(int) Intervals.numElements(new FinalInterval(dims))];
+
+		if (fill) {
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (byte) 1;
+			}
+		}
+
+		return ArrayImgs.bytes(array, dims);
+	}
+
+	private int seed;
+	
+	private int pseudoRandom() {
+		return seed = 3170425 * seed + 132102;
+	}
+
+	public ArrayImg<ByteType, ByteArray> generatePositiveByteArrayTestImg(final boolean fill,
+		final long... dims)
+	{
+		final byte[] array =
+			new byte[(int) Intervals.numElements(new FinalInterval(dims))];
+
+		if (fill) {
+			seed = 17;
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (byte) randInt(0, 100);
+			}
+		}
+
+		return ArrayImgs.bytes(array, dims);
+	}
+
+	public static int randInt(int min, int max) {
+    // NOTE: This will (intentionally) not run as written so that folks
+    // copy-pasting have to think about how to initialize their
+    // Random instance.  Initialization of the Random instance is outside
+    // the main scope of the question, but some decent options are to have
+    // a field that is initialized once and then re-used as needed or to
+    // use ThreadLocalRandom (if using at least Java 1.7).
+    Random rand = new Random();
+
+    // nextInt is normally exclusive of the top value,
+    // so add 1 to make it inclusive
+    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+    return randomNum;
+	}
+
+	public ArrayImg<ByteType, ByteArray> generateKnownByteArrayTestImgSmall()
+	{
+		final long[] dims = new long[]{2, 2};
+		final byte[] array = new byte[4];
+		
+		array[0] = (byte) 10;
+		array[1] = (byte) 20;
+		array[2] = (byte) 30;
+		array[3] = (byte) 40;
+
+		return ArrayImgs.bytes(array, dims);
+	}
+
+	public ArrayImg<ByteType, ByteArray> generateKnownByteArrayTestImgLarge()
+	{
+		final long[] dims = new long[]{3, 3};
+		final byte[] array = new byte[9];
+		
+		array[0] = (byte) 40;
+		array[1] = (byte) 40;
+		array[2] = (byte) 20;
+		
+		array[3] = (byte) 40;
+		array[4] = (byte) 40;
+		array[5] = (byte) 20;
+		
+		array[6] = (byte) 20;
+		array[7] = (byte) 20;
+		array[8] = (byte) 100;
+		
+		return ArrayImgs.bytes(array, dims);
 	}
 
 }
