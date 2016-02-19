b3293aa56d495f0c974726064a0dca62795b941c
LeonYang5114 <leonyang1994@gmail.com>
2016-02-19 10:58:51 -0600

    Create fill ops in image namespace
    A fill op fills an InterableInterval with some constant value.

---
+++ src/main/java/net/imagej/ops/image/fill/DefaultFill.java
@@ -0,0 +1,70 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.image.fill;
+
+import net.imagej.ops.Ops;
+import net.imagej.ops.special.computer.AbstractNullaryComputerOp;
+import net.imagej.ops.special.computer.Computers;
+import net.imagej.ops.special.computer.NullaryComputerOp;
+import net.imglib2.IterableInterval;
+import net.imglib2.type.Type;
+
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Fill an {@link IterableInterval} with some constant value.
+ * 
+ * @author Leon Yang
+ */
+@Plugin(type = Ops.Image.Fill.class)
+public class DefaultFill<T extends Type<T>> extends
+	AbstractNullaryComputerOp<Iterable<T>> implements Ops.Image.Fill
+{
+
+	@Parameter
+	private T constant;
+
+	private NullaryComputerOp<T> assigner;
+	private NullaryComputerOp<Iterable<T>> map;
+
+	@Override
+	public void initialize() {
+		assigner = Computers.nullary(ops(), Ops.Math.Assign.class, constant
+			.createVariable(), constant);
+		map = Computers.nullary(ops(), Ops.Map.class, out(), assigner);
+	}
+
+	@Override
+	public void compute0(final Iterable<T> output) {
+		map.compute0(output);
+	}
+}
