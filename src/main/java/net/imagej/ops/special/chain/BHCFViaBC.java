d0f453980c6db4d6755191d31673c85e4f58b6ba
Curtis Rueden <ctrueden@wisc.edu>
2016-04-27 12:22:20 -0500

    Add missing binary delegating ops
    This includes missing SameIO and AllSame ops,
    as well as BHCFViaBC and BIOViaBI ops.
    
    See previous commit for details.

---
+++ src/main/java/net/imagej/ops/special/chain/BHCFViaBC.java
@@ -0,0 +1,71 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.special.chain;
+
+import net.imagej.ops.special.BinaryOutputFactory;
+import net.imagej.ops.special.computer.BinaryComputerOp;
+import net.imagej.ops.special.hybrid.AbstractBinaryHybridCF;
+import net.imagej.ops.special.hybrid.BinaryHybridCF;
+
+/**
+ * Base class for {@link BinaryHybridCF}s that delegate to
+ * {@link BinaryComputerOp}s.
+ * <p>
+ * Implementing classes will need to provide the missing
+ * {@link BinaryOutputFactory#createOutput} method implementation.
+ * </p>
+ *
+ * @author Curtis Rueden
+ * @param <I1> type of first input
+ * @param <I2> type of second input
+ * @param <O> type of output
+ * @param <DI1> type of first input accepted by the worker op
+ * @param <DI2> type of second input accepted by the worker op
+ * @param <DO> type of output accepted by the worker op
+ */
+public abstract class BHCFViaBC<I1 extends DI1, I2 extends DI2, O extends DO, DI1, DI2, DO>
+	extends AbstractBinaryHybridCF<I1, I2, O> implements
+	DelegatingBinaryOp<I1, I2, O, DI1, DI2, DO, BinaryComputerOp<DI1, DI2, DO>>
+{
+
+	private BinaryComputerOp<DI1, DI2, DO> worker;
+
+	@Override
+	public void initialize() {
+		worker = createWorker(in1(), in2());
+	}
+
+	@Override
+	public void compute2(final I1 input1, final I2 input2, final O output) {
+		worker.compute2(input1, input2, output);
+	}
+
+}
