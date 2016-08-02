b75d73a5ecec8c5d0e3c3be290b431b9a8271700
Curtis Rueden <ctrueden@wisc.edu>
2016-08-02 08:53:00 -0500

    Update license headers and add missing ones
    Accomplished using:
      mvn license:update-file-header

--- src/test/java/net/imagej/ops/image/fill/FillTest.java
+++ src/test/java/net/imagej/ops/image/fill/FillTest.java
@@ -5,13 +5,13 @@
  * Copyright (C) 2014 - 2016 Board of Regents of the University of
  * Wisconsin-Madison, University of Konstanz and Brian Northan.
  * %%
- * Redistribution and use out source and binary forms, with or without
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
- * 2. Redistributions out binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer out the documentation
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
