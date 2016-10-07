147c57cc2b74009c10894c4c39ec283aad4a157b
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2016-10-07 13:45:30 +0200

    Add superclass for histogram-based thresholders

---
+++ src/main/java/net/imagej/ops/threshold/ConstantThresholder.java
@@ -0,0 +1,60 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.threshold;
+
+import net.imagej.ops.special.computer.AbstractUnaryComputerOp;
+import net.imglib2.type.BooleanType;
+
+/**
+ * Uses a constant threshold to determine a boolean output for an input value.
+ * TODO Rename class (clashes with LocalThresholder and GlobalThresholder)
+ * 
+ * @author Christian Dietz (University of Konstanz)
+ * @author Stefan Helfrich (University of Konstanz)
+ * @param <I> type of input
+ * @param <O> type of output
+ */
+public class ConstantThresholder<I extends Comparable<I>, O extends BooleanType<O>>
+	extends AbstractUnaryComputerOp<I, O>
+{
+
+	private I threshold;
+
+	public ConstantThresholder(I threshold) {
+		this.threshold = threshold;
+	}
+
+	@Override
+	public void compute1(I input, O output) {
+		output.set(threshold.compareTo(input) > 0);
+	}
+
+}
