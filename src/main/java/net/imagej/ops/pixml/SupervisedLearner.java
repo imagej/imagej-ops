8ff2566b391141e81aad86abcb4e90848415d5aa
Stefan Helfrich <stefan.helfrich@uni-konstanz.de>
2016-10-07 10:40:36 +0200

    Add interfaces for pixel-based learners
    Both SupervisedLearners and UnsupervisedLearners are learning a model
    from input data that can be applied to individual pixels. That is, a
    model is a UnaryComputerOp that computes and output value for an input
    pixel.
    
    A simple example for an UnsupervisedLearner is thresholding. Given an
    image (with help of the histogram), generate a UnaryComputerOp that
    compares the pixel value to the computed threshold and sets the output
    pixel "true" or "false" (output is of BooleanType).

---
+++ src/main/java/net/imagej/ops/pixml/SupervisedLearner.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package net.imagej.ops.pixml;
+
+import net.imagej.ops.special.computer.UnaryComputerOp;
+import net.imagej.ops.special.function.BinaryFunctionOp;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessible;
+
+/**
+ * Learns a <em>model</em> from an {@link IterableInterval} and a labeling.
+ * <p>
+ * A <em>model</em> is a {@link UnaryComputerOp} that given an input computes an
+ * output value. The internals of the model have been learned from the input
+ * {@code IterableInterval}.
+ * </p>
+ * 
+ * @author Christian Dietz (University of Konstanz)
+ * @author Martin Horn (University of Konstanz)
+ * @author Stefan Helfrich (University of Konstanz)
+ * @param <I> type of input
+ * @param <L> type of labeling
+ * @param <O> type of output
+ */
+public interface SupervisedLearner<I, L, O> extends
+	BinaryFunctionOp<IterableInterval<I>, RandomAccessible<L>, UnaryComputerOp<I, O>>
+{
+	// NB: marker interface
+}
