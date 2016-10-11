5a77de35abe658a94fbd68b3ae248e72154000ce
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2016-10-11 14:50:35 +0200

    Fix elongation implementation.

--- src/main/java/net/imagej/ops/geom/geom2d/DefaultElongation.java
+++ src/main/java/net/imagej/ops/geom/geom2d/DefaultElongation.java
@@ -30,36 +30,56 @@
 
 package net.imagej.ops.geom.geom2d;
 
+import java.util.List;
+
 import net.imagej.ops.Ops;
-import net.imagej.ops.special.chain.RTs;
+import net.imagej.ops.special.function.Functions;
 import net.imagej.ops.special.function.UnaryFunctionOp;
 import net.imagej.ops.special.hybrid.AbstractUnaryHybridCF;
+import net.imglib2.RealLocalizable;
 import net.imglib2.roi.geometric.Polygon;
 import net.imglib2.type.numeric.real.DoubleType;
 
 import org.scijava.plugin.Plugin;
 
 /**
- * Generic implementation of {@code geom.mainElongation}.
+ * Generic implementation of {@code geom.mainElongation} based on
+ * http://www.math.uci.edu/icamp/summer/research_11/park/
+ * shape_descriptors_survey.pdf.
  * 
- * @author Daniel Seebacher (University of Konstanz)
+ * @author Tim-Oliver Buchholz, University of Konstanz
  */
 @Plugin(type = Ops.Geometric.MainElongation.class, label = "Geometric (2D): Elongation")
 public class DefaultElongation extends AbstractUnaryHybridCF<Polygon, DoubleType>
 		implements Ops.Geometric.MainElongation {
 
-	private UnaryFunctionOp<Polygon, DoubleType> minorAxisFunc;
-	private UnaryFunctionOp<Polygon, DoubleType> majorAxisFunc;
+	private UnaryFunctionOp<Polygon, Polygon> minimumBoundingBoxFunc;
 
 	@Override
 	public void initialize() {
-		minorAxisFunc = RTs.function(ops(), Ops.Geometric.MinorAxis.class, in());
-		majorAxisFunc = RTs.function(ops(), Ops.Geometric.MajorAxis.class, in());
+		minimumBoundingBoxFunc = Functions.unary(ops(), Ops.Geometric.SmallestEnclosingBoundingBox.class, Polygon.class,
+				in());
 	}
 
 	@Override
 	public void compute1(final Polygon input, final DoubleType output) {
-		output.set(1d - minorAxisFunc.compute1(input).getRealDouble() / majorAxisFunc.compute1(input).getRealDouble());
+		final List<? extends RealLocalizable> minBB = minimumBoundingBoxFunc.compute1(input).getVertices();
+
+		final RealLocalizable p1 = minBB.get(0);
+		final RealLocalizable p2 = minBB.get(1);
+		final RealLocalizable p3 = minBB.get(2);
+
+		double width = Math.sqrt(Math.pow(p1.getDoublePosition(0) - p2.getDoublePosition(0), 2)
+				+ Math.pow(p1.getDoublePosition(1) - p2.getDoublePosition(1), 2));
+		double length = Math.sqrt(Math.pow(p2.getDoublePosition(0) - p3.getDoublePosition(0), 2)
+				+ Math.pow(p2.getDoublePosition(1) - p3.getDoublePosition(1), 2));
+
+		if (width > length) {
+			double tmp = width;
+			width = length;
+			length = tmp;
+		}
+		output.set(1d - (width / length));
 	}
 
 	@Override
