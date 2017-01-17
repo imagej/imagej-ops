34fb9e26e5af528d3dc14c9270286c3b099257be
Curtis Rueden <ctrueden@wisc.edu>
2017-01-17 09:51:22 -0600

    Happy New Year 2017

--- src/main/java/net/imagej/ops/create/kernelLog/CreateKernelLogSymmetric.java
+++ src/main/java/net/imagej/ops/create/kernelLog/CreateKernelLogSymmetric.java
@@ -5,7 +5,7 @@ package net.imagej.ops.create.kernelLog;
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Copyright (C) 2014 - 2017 Board of Regents of the University of
  * Wisconsin-Madison, University of Konstanz and Brian Northan.
  * %%
  * Redistribution and use in source and binary forms, with or without
--- src/main/java/net/imagej/ops/special/hybrid/BinaryHybridCFI1.java
+++ src/main/java/net/imagej/ops/special/hybrid/BinaryHybridCFI1.java
@@ -2,7 +2,7 @@
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Copyright (C) 2014 - 2017 Board of Regents of the University of
  * Wisconsin-Madison, University of Konstanz and Brian Northan.
  * %%
  * Redistribution and use in source and binary forms, with or without
