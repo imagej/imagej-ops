04a3afa84296be9889755dad3076065ad2ba70bb
Curtis Rueden <ctrueden@wisc.edu>
2017-06-06 14:56:57 -0500

    DefaultGaussRAI: clean up style

--- src/main/java/net/imagej/ops/filter/gauss/DefaultGaussRAI.java
+++ src/main/java/net/imagej/ops/filter/gauss/DefaultGaussRAI.java
@@ -7,13 +7,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -51,7 +51,7 @@ import org.scijava.thread.ThreadService;
 
 /**
  * Gaussian filter, wrapping {@link Gauss3} of imglib2-algorithms.
- * 
+ *
  * @author Christian Dietz (University of Konstanz)
  * @param <T> type of input and output
  */
@@ -76,18 +76,18 @@ public class DefaultGaussRAI<T extends RealType<T> & NativeType<T>> extends
 		final RandomAccessibleInterval<T> output)
 	{
 
-		if (outOfBounds == null) outOfBounds =
-			new OutOfBoundsMirrorFactory<>(
-				Boundary.SINGLE);
+		if (outOfBounds == null) {
+			outOfBounds = new OutOfBoundsMirrorFactory<>(Boundary.SINGLE);
+		}
 
-		final RandomAccessible<FloatType> eIn =
+		final RandomAccessible<FloatType> eIn = //
 			(RandomAccessible) Views.extend(input, outOfBounds);
 
 		try {
 			SeparableSymmetricConvolution.convolve(Gauss3.halfkernels(sigmas), eIn,
 				output, threads.getExecutorService());
 		}
-		catch (IncompatibleTypeException e) {
+		catch (final IncompatibleTypeException e) {
 			throw new RuntimeException(e);
 		}
 	}
