bf73b8ed34d91669b4a11a496cd903af9c4f617e
Curtis Rueden <ctrueden@wisc.edu>
2017-06-06 14:59:48 -0500

    GaussRAISingleSigma: clean up style

--- src/main/java/net/imagej/ops/filter/gauss/GaussRAISingleSigma.java
+++ src/main/java/net/imagej/ops/filter/gauss/GaussRAISingleSigma.java
@@ -7,13 +7,13 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -48,7 +48,7 @@ import org.scijava.plugin.Plugin;
 /**
  * Gaussian filter which can be called with single sigma, i.e. the sigma is the
  * same in each dimension.
- * 
+ *
  * @author Christian Dietz (University of Konstanz)
  * @param <T> type of input
  */
@@ -72,7 +72,7 @@ public class GaussRAISingleSigma<T extends RealType<T> & NativeType<T>> extends
 		Arrays.fill(sigmas, sigma);
 		gaussOp = RAIs.computer(ops(), Gauss.class, in(), sigmas, outOfBounds);
 	}
-	
+
 	@Override
 	public void compute(final RandomAccessibleInterval<T> input,
 		final RandomAccessibleInterval<T> output)
