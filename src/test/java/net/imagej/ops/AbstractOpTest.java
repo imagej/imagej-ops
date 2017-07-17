9b593df47c40b40f0d013710135197e8c0ab27ca
gselzer <gabeselzer@gmail.com>
2017-07-17 13:29:31 -0500

    Add methods for Img generation for all types
    This commit creates Imgs for testing for all RealTypes.

--- src/test/java/net/imagej/ops/AbstractOpTest.java
+++ src/test/java/net/imagej/ops/AbstractOpTest.java
@@ -35,11 +35,13 @@ import static org.junit.Assert.assertTrue;
 
 import io.scif.img.IO;
 
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.stream.StreamSupport;
 
+import net.imagej.types.UnboundedIntegerType;
 import net.imglib2.Cursor;
 import net.imglib2.FinalInterval;
 import net.imglib2.IterableInterval;
@@ -57,12 +59,15 @@ import net.imglib2.img.basictypeaccess.array.LongArray;
 import net.imglib2.img.basictypeaccess.array.ShortArray;
 import net.imglib2.img.cell.CellImg;
 import net.imglib2.img.cell.CellImgFactory;
+import net.imglib2.img.list.ListImg;
+import net.imglib2.img.list.ListImgFactory;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.type.numeric.integer.ShortType;
+import net.imglib2.type.numeric.integer.Unsigned128BitType;
 import net.imglib2.type.numeric.integer.Unsigned12BitType;
 import net.imglib2.type.numeric.integer.Unsigned2BitType;
 import net.imglib2.type.numeric.integer.Unsigned4BitType;
@@ -70,6 +75,7 @@ import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.imglib2.type.numeric.integer.UnsignedIntType;
 import net.imglib2.type.numeric.integer.UnsignedLongType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.integer.UnsignedVariableBitLengthType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Intervals;
@@ -130,16 +136,15 @@ public abstract class AbstractOpTest {
 
 	private int seed;
 
-
 	private int pseudoRandom() {
 		return seed = 3170425 * seed + 132102;
 	}
 
-	public ArrayImg<BitType, LongArray> generateBitArrayTestImg(final boolean fill,
-		final long... dims)
+	public ArrayImg<BitType, LongArray> generateBitArrayTestImg(
+		final boolean fill, final long... dims)
 	{
 		ArrayImg<BitType, LongArray> bits = ArrayImgs.bits(dims);
-		
+
 		if (fill) {
 			MersenneTwisterFast betterRNG = new MersenneTwisterFast(0xf1eece);
 			for (BitType b : bits) {
@@ -148,7 +153,7 @@ public abstract class AbstractOpTest {
 		}
 		return bits;
 	}
-	
+
 	public ArrayImg<Unsigned2BitType, LongArray> generateUnsigned2BitArrayTestImg(
 		final boolean fill, final long... dims)
 	{
@@ -162,7 +167,7 @@ public abstract class AbstractOpTest {
 		}
 		return bits;
 	}
-	
+
 	public ArrayImg<Unsigned4BitType, LongArray> generateUnsigned4BitArrayTestImg(
 		final boolean fill, final long... dims)
 	{
@@ -176,11 +181,12 @@ public abstract class AbstractOpTest {
 		}
 		return bits;
 	}
-	
-	public ArrayImg<Unsigned12BitType, LongArray> generateUnsigned12BitArrayTestImg(
-		final boolean fill, final long... dims)
+
+	public ArrayImg<Unsigned12BitType, LongArray>
+		generateUnsigned12BitArrayTestImg(final boolean fill, final long... dims)
 	{
-		ArrayImg<Unsigned12BitType, LongArray> bits = ArrayImgs.unsigned12Bits(dims);
+		ArrayImg<Unsigned12BitType, LongArray> bits = ArrayImgs.unsigned12Bits(
+			dims);
 
 		if (fill) {
 			MersenneTwisterFast betterRNG = new MersenneTwisterFast(0xf1eece);
@@ -190,9 +196,25 @@ public abstract class AbstractOpTest {
 		}
 		return bits;
 	}
-	
-	public ArrayImg<ByteType, ByteArray> generateByteArrayTestImg(final boolean fill,
-		final long... dims)
+
+	public ArrayImg<Unsigned128BitType, LongArray>
+		generateUnsigned128BitArrayTestImg(final boolean fill, final long... dims)
+	{
+		ArrayImg<Unsigned128BitType, LongArray> bits = ArrayImgs.unsigned128Bits(
+			dims);
+
+		if (fill) {
+			MersenneTwisterFast betterRNG = new MersenneTwisterFast(0xf1eece);
+			for (Unsigned128BitType b : bits) {
+				BigInteger big = BigInteger.valueOf(betterRNG.nextLong());
+				b.set(big);
+			}
+		}
+		return bits;
+	}
+
+	public ArrayImg<ByteType, ByteArray> generateByteArrayTestImg(
+		final boolean fill, final long... dims)
 	{
 		final byte[] array = new byte[(int) Intervals.numElements(new FinalInterval(
 			dims))];
@@ -222,30 +244,36 @@ public abstract class AbstractOpTest {
 
 		return ArrayImgs.unsignedBytes(array, dims);
 	}
-	
-	public ArrayImg<IntType, IntArray> generateIntArrayTestImg(final boolean fill, final long... dims){
-		final int[] array = new int[(int) Intervals.numElements(new FinalInterval(dims))];
-		
-		if(fill) {
+
+	public ArrayImg<IntType, IntArray> generateIntArrayTestImg(final boolean fill,
+		final long... dims)
+	{
+		final int[] array = new int[(int) Intervals.numElements(new FinalInterval(
+			dims))];
+
+		if (fill) {
 			seed = 17;
-			for(int i = 0; i < array.length; i++){
+			for (int i = 0; i < array.length; i++) {
 				array[i] = (int) pseudoRandom() / (int) Integer.MAX_VALUE;
 			}
 		}
-		
+
 		return ArrayImgs.ints(array, dims);
 	}
-	
-	public ArrayImg<UnsignedIntType, IntArray> generateUnsignedIntArrayTestImg(final boolean fill, final long... dims){
-		final int[] array = new int[(int) Intervals.numElements(new FinalInterval(dims))];
-		
-		if(fill) {
+
+	public ArrayImg<UnsignedIntType, IntArray> generateUnsignedIntArrayTestImg(
+		final boolean fill, final long... dims)
+	{
+		final int[] array = new int[(int) Intervals.numElements(new FinalInterval(
+			dims))];
+
+		if (fill) {
 			seed = 17;
-			for(int i = 0; i < array.length; i++){
+			for (int i = 0; i < array.length; i++) {
 				array[i] = (int) pseudoRandom() / (int) Integer.MAX_VALUE;
 			}
 		}
-		
+
 		return ArrayImgs.unsignedInts(array, dims);
 	}
 
@@ -278,12 +306,12 @@ public abstract class AbstractOpTest {
 
 		return img;
 	}
-	
+
 	public ArrayImg<DoubleType, DoubleArray> generateDoubleArrayTestImg(
 		final boolean fill, final long... dims)
 	{
-		final double[] array =
-			new double[(int) Intervals.numElements(new FinalInterval(dims))];
+		final double[] array = new double[(int) Intervals.numElements(
+			new FinalInterval(dims))];
 
 		if (fill) {
 			seed = 17;
@@ -294,57 +322,113 @@ public abstract class AbstractOpTest {
 
 		return ArrayImgs.doubles(array, dims);
 	}
-	
-	public ArrayImg<LongType, LongArray> generateLongArrayTestImg(final boolean fill, final long... dims){
-		final long[] array = new long[(int) Intervals.numElements(new FinalInterval(dims))];
-		
-		if(fill) {
+
+	public ArrayImg<LongType, LongArray> generateLongArrayTestImg(
+		final boolean fill, final long... dims)
+	{
+		final long[] array = new long[(int) Intervals.numElements(new FinalInterval(
+			dims))];
+
+		if (fill) {
 			seed = 17;
-			for(int i = 0; i < array.length; i++){
-				array[i] =  (long) (pseudoRandom() /  Integer.MAX_VALUE);
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (long) (pseudoRandom() / Integer.MAX_VALUE);
 			}
 		}
-		
+
 		return ArrayImgs.longs(array, dims);
 	}
-	
-	public ArrayImg<UnsignedLongType, LongArray> generateUnsignedLongArrayTestImg(final boolean fill, final long... dims){
-		final long[] array = new long[(int) Intervals.numElements(new FinalInterval(dims))];
-		
-		if(fill) {
+
+	public ArrayImg<UnsignedLongType, LongArray> generateUnsignedLongArrayTestImg(
+		final boolean fill, final long... dims)
+	{
+		final long[] array = new long[(int) Intervals.numElements(new FinalInterval(
+			dims))];
+
+		if (fill) {
 			seed = 17;
-			for(int i = 0; i < array.length; i++){
-				array[i] =  (long) (pseudoRandom() /  Integer.MAX_VALUE);
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (long) (pseudoRandom() / Integer.MAX_VALUE);
 			}
 		}
-		
+
 		return ArrayImgs.unsignedLongs(array, dims);
 	}
-	
-	public ArrayImg<ShortType, ShortArray> generateShortArrayTestImg(final boolean fill, final long... dims){
-		final short[] array = new short[(int) Intervals.numElements(new FinalInterval(dims))];
-		
-		if(fill) {
+
+	public ArrayImg<ShortType, ShortArray> generateShortArrayTestImg(
+		final boolean fill, final long... dims)
+	{
+		final short[] array = new short[(int) Intervals.numElements(
+			new FinalInterval(dims))];
+
+		if (fill) {
 			seed = 17;
-			for(int i = 0; i < array.length; i++){
-				array[i] =  (short) (pseudoRandom() /  Integer.MAX_VALUE);
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (short) (pseudoRandom() / Integer.MAX_VALUE);
 			}
 		}
-		
+
 		return ArrayImgs.shorts(array, dims);
 	}
-	
-	public ArrayImg<UnsignedShortType, ShortArray> generateUnsignedShortArrayTestImg(final boolean fill, final long... dims){
-		final short[] array = new short[(int) Intervals.numElements(new FinalInterval(dims))];
-		
-		if(fill) {
+
+	public ArrayImg<UnsignedShortType, ShortArray>
+		generateUnsignedShortArrayTestImg(final boolean fill, final long... dims)
+	{
+		final short[] array = new short[(int) Intervals.numElements(
+			new FinalInterval(dims))];
+
+		if (fill) {
 			seed = 17;
-			for(int i = 0; i < array.length; i++){
-				array[i] =  (short) (pseudoRandom() /  Integer.MAX_VALUE);
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (short) (pseudoRandom() / Integer.MAX_VALUE);
 			}
 		}
-		
+
 		return ArrayImgs.unsignedShorts(array, dims);
+	}
+
+	public ArrayImg<UnsignedVariableBitLengthType, LongArray>
+		generateUnsignedVariableBitLengthTypeArrayTestImg(final boolean fill,
+			final int nbits, final long... dims)
+	{
+		final long[] array = new long[(int) Intervals.numElements(new FinalInterval(
+			dims))];
+
+		if (fill) {
+			seed = 17;
+			for (int i = 0; i < array.length; i++) {
+				array[i] = (long) (pseudoRandom() / Integer.MAX_VALUE);
+			}
+		}
+
+		final LongArray l = new LongArray(array);
+
+		return ArrayImgs.unsignedVariableBitLengths(l, nbits, dims);
+	}
+
+	public ListImg<UnboundedIntegerType> generateUnboundedIntegerTypeListTestImg(
+		final boolean fill, final long... dims)
+	{
+
+		final ListImg<UnboundedIntegerType> l =
+			new ListImgFactory<UnboundedIntegerType>().create(dims,
+				new UnboundedIntegerType());
+
+		final BigInteger[] array = new BigInteger[(int) Intervals.numElements(
+			dims)];
+
+		RandomAccess<UnboundedIntegerType> ra = l.randomAccess();
+
+		if (fill) {
+			MersenneTwisterFast betterRNG = new MersenneTwisterFast(0xf1eece);
+			for (int i = 0; i < Intervals.numElements(dims); i++) {
+				BigInteger val = BigInteger.valueOf(betterRNG.nextLong());
+				ra.get().set(val);
+				ra.fwd(0);
+			}
+		}
+
+		return l;
 	}
 
 	public Img<UnsignedByteType>
