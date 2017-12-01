ff6156653b9d5df2f225011d6cb31cc4a590e640
Brian Northan <bnorthan@gmail.com>
2017-12-01 09:04:00 -0500

    Make ifft computer really a computer
    IFFT computer tries to save memory by writing temporary IFFT
    calculations to the input image. This means the computer changes the
    input. We can avoid this by making a copy of the input and using that
    for the temporary calculations.
    
    This will make the op use more memory and perform slower. In the future
    an op type which allows using the input img for temporary calculations
    should be created.
    
    Signed-off-by: Curtis Rueden <ctrueden@wisc.edu>

--- src/main/java/net/imagej/ops/filter/ifft/IFFTMethodsOpC.java
+++ src/main/java/net/imagej/ops/filter/ifft/IFFTMethodsOpC.java
@@ -31,7 +31,9 @@ package net.imagej.ops.filter.ifft;
 
 import net.imagej.ops.Contingent;
 import net.imagej.ops.Ops;
+import net.imagej.ops.special.chain.RAIs;
 import net.imagej.ops.special.computer.AbstractUnaryComputerOp;
+import net.imagej.ops.special.function.UnaryFunctionOp;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.fft2.FFTMethods;
 import net.imglib2.type.numeric.ComplexType;
@@ -61,6 +63,14 @@ public class IFFTMethodsOpC<C extends ComplexType<C>, T extends RealType<T>>
 	@Parameter
 	ThreadService ts;
 
+	private UnaryFunctionOp<RandomAccessibleInterval<C>, RandomAccessibleInterval<C>> copyOp;
+
+	@Override
+	public void initialize() {
+		super.initialize();
+		copyOp = RAIs.function(ops(), Ops.Copy.RAI.class, in());
+	}
+
 	/**
 	 * Compute an ND inverse FFT
 	 */
@@ -68,13 +78,14 @@ public class IFFTMethodsOpC<C extends ComplexType<C>, T extends RealType<T>>
 	public void compute(final RandomAccessibleInterval<C> input,
 		final RandomAccessibleInterval<T> output)
 	{
+		final RandomAccessibleInterval<C> temp = copyOp.calculate(input);
+
 		for (int d = input.numDimensions() - 1; d > 0; d--)
-			FFTMethods.complexToComplex(input, d, false, true, ts
+			FFTMethods.complexToComplex(temp, d, false, true, ts
 				.getExecutorService());
 
-		FFTMethods.complexToReal(input, output, FFTMethods
-			.unpaddingIntervalCentered(input, output), 0, true, ts
-				.getExecutorService());
+		FFTMethods.complexToReal(temp, output, FFTMethods.unpaddingIntervalCentered(
+			temp, output), 0, true, ts.getExecutorService());
 	}
 
 	/**
