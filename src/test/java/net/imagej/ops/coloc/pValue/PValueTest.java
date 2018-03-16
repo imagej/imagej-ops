333e9f027f1605dc15bcc24c5ef7f239d80ed538
Ellen T Arena <arenaellen@gmail.com>
2018-03-16 13:58:47 -0500

    PValueTest: minor edits, fixed lambda expressions

--- src/test/java/net/imagej/ops/coloc/pValue/PValueTest.java
+++ src/test/java/net/imagej/ops/coloc/pValue/PValueTest.java
@@ -56,39 +56,43 @@ public class PValueTest extends ColocalisationTest {
 	public void testPValuePerfectColoc() {
 		assertColoc(0.0, 1, 0, 0, 0, 0, 0, 0);
 	}
+
 	@Test
 	public void testPValueNoColoc() {
 		assertColoc(1.0, 0, 1, 2, 3, 4, 5);
 	}
+
 	@Test
 	public void testPValueSomeColoc() {
 		assertColoc(0.6, 0.25, 0.25, 0.25, 0.75, 0.75, 0.75);
 	}
 
-/**
- * Function is called once with original images. Thereafter, 
- * each call is with a shuffled version of the first image.
- * @param expectedPValue
- * @param result
- */
+	/**
+	 * Function is called once with original images. Thereafter, each call is with
+	 * a shuffled version of the first image.
+	 * 
+	 * @param expectedPValue
+	 * @param result
+	 */
 	private void assertColoc(double expectedPValue, double... result) {
 		Img<FloatType> ch1 = ArrayImgs.floats(1); // NB: Images will be ignored.
 		Img<FloatType> ch2 = ch1;
 
 		// Mock the underlying op.
-		final int[] count = {0};
-		BinaryFunctionOp<Iterable<FloatType>, Iterable<FloatType>, Double> op = op((input1, input2) -> {
-			return result[count[0]++];
-		});
+		final int[] count = { 0 };
+		BinaryFunctionOp<Iterable<FloatType>, Iterable<FloatType>, Double> op = //
+			op((input1, input2) -> result[count[0]++]);
 		Double actualPValue = ops.coloc().pValue(ch1, ch2, op, result.length - 1);
 		assertEquals(expectedPValue, actualPValue, 0.0);
 	}
 
 	// -- Utility methods --
 
-	private static <I1, I2, O> BinaryFunctionOp<I1, I2, O> op(final BiFunction<I1, I2, O> function) {		
+	private static <I1, I2, O> BinaryFunctionOp<I1, I2, O> op(
+		final BiFunction<I1, I2, O> function)
+	{
 		return new AbstractBinaryFunctionOp<I1, I2, O>() {
-	
+
 			@Override
 			public O calculate(final I1 input1, final I2 input2) {
 				return function.apply(input1, input2);
