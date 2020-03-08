4e65d42fe9ac4cecdf0b1f3e3d65498e2ec77e48
rimadoma <richard.domander@gmail.com>
2020-03-08 00:10:20 +0000

    Optimise Outline op
    Execution time for a 256 x 256 x 256 image drops from about 2 s to 500
    ms.

--- src/main/java/net/imagej/ops/morphology/outline/Outline.java
+++ src/main/java/net/imagej/ops/morphology/outline/Outline.java
@@ -29,8 +29,6 @@
 
 package net.imagej.ops.morphology.outline;
 
-import java.util.Arrays;
-
 import net.imagej.ops.Ops;
 import net.imagej.ops.special.hybrid.AbstractBinaryHybridCF;
 import net.imglib2.Cursor;
@@ -38,12 +36,10 @@ import net.imglib2.FinalDimensions;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.outofbounds.OutOfBounds;
-import net.imglib2.roi.labeling.BoundingBox;
 import net.imglib2.type.BooleanType;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.util.Util;
 import net.imglib2.view.ExtendedRandomAccessibleInterval;
-import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
 import org.scijava.plugin.Plugin;
@@ -73,7 +69,7 @@ public class Outline<B extends BooleanType<B>> extends
 	/**
 	 * Copies the outlines of the objects in the input interval into the output
 	 *
-	 * @param input a binary interval
+	 * @param input an N-dimensional binary interval
 	 * @param excludeEdges are elements on stack edges outline or not
 	 *          <p>
 	 *          For example, a 2D square:<br>
@@ -92,15 +88,15 @@ public class Outline<B extends BooleanType<B>> extends
 		final RandomAccessibleInterval<BitType> output)
 	{
 		final Cursor<B> inputCursor = Views.iterable(input).localizingCursor();
-		final long[] coordinates = new long[input.numDimensions()];
-		final ExtendedRandomAccessibleInterval<B, RandomAccessibleInterval<B>> extendedInput =
-			extendInterval(input);
+		final long[] inputPosition = new long[input.numDimensions()];
+		final OutOfBounds<B> neighbourhoodAccess = extendInterval(input).randomAccess();
 		final RandomAccess<BitType> outputAccess = output.randomAccess();
 		while (inputCursor.hasNext()) {
 			inputCursor.fwd();
-			inputCursor.localize(coordinates);
-			if (isOutline(extendedInput, coordinates)) {
-				outputAccess.setPosition(coordinates);
+			inputCursor.localize(inputPosition);
+			neighbourhoodAccess.setPosition(inputPosition);
+			if (isOutline(neighbourhoodAccess, inputPosition)) {
+				outputAccess.setPosition(inputPosition);
 				outputAccess.get().set(inputCursor.get().get());
 			}
 		}
@@ -115,36 +111,19 @@ public class Outline<B extends BooleanType<B>> extends
 		return Views.extendValue(interval, type);
 	}
 
-	/**
-	 * Creates a view that spans from (x-1, y-1, ... i-1) to (x+1, y+1, ... i+1)
-	 * around the given coordinates
-	 *
-	 * @param interval the space of the coordinates
-	 * @param coordinates coordinates (x, y, ... i)
-	 * @return a view of a neighbourhood in the space
-	 */
-	private IntervalView<B> neighbourhoodInterval(
-		final ExtendedRandomAccessibleInterval<B, RandomAccessibleInterval<B>> interval,
-		final long[] coordinates)
-	{
-		final int dimensions = interval.numDimensions();
-		final BoundingBox box = new BoundingBox(dimensions);
-		final long[] minBounds = Arrays.stream(coordinates).map(c -> c - 1)
-			.toArray();
-		final long[] maxBounds = Arrays.stream(coordinates).map(c -> c + 1)
-			.toArray();
-		box.update(minBounds);
-		box.update(maxBounds);
-		return Views.offsetInterval(interval, box);
-	}
-
-	/** Checks if any element in the neighbourhood is background */
-	private boolean isAnyBackground(final IntervalView<B> neighbourhood) {
-		final Cursor<B> cursor = neighbourhood.cursor();
-		while (cursor.hasNext()) {
-			cursor.fwd();
-			if (!cursor.get().get()) {
-				return true;
+	/** Checks if any element in the N-dimensional neighbourhood is background */
+	private boolean isAnyNeighborBackground(final int dimension, final OutOfBounds<B> access,
+											final long[] position) {
+		for (long p = -1; p <= 1; p++) {
+			access.setPosition(position[dimension] + p, dimension);
+			if (dimension == 0) {
+				if (!access.get().get()) {
+					return true;
+				}
+			} else {
+				if (isAnyNeighborBackground(dimension - 1, access, position)) {
+					return true;
+				}
 			}
 		}
 		return false;
@@ -153,24 +132,15 @@ public class Outline<B extends BooleanType<B>> extends
 	/**
 	 * Checks if an element is part of the outline of an object
 	 *
-	 * @param source the location of the element
-	 * @param coordinates coordinates of the element
+	 * @param access an access to an extended view of the input interval
+	 * @param position current position in the input image
 	 * @return true if element is foreground and has at least one background
 	 *         neighbour
 	 */
-	private boolean isOutline(
-		final ExtendedRandomAccessibleInterval<B, RandomAccessibleInterval<B>> source,
-		final long[] coordinates)
+	private boolean isOutline(final OutOfBounds<B> access, final long[] position)
 	{
-		final OutOfBounds<B> access = source.randomAccess();
-		access.setPosition(coordinates);
-		if (!access.get().get()) {
-			return false;
-		}
-
-		final IntervalView<B> neighbourhood = neighbourhoodInterval(source,
-			coordinates);
-		return isAnyBackground(neighbourhood);
+		final int dimension = access.numDimensions() - 1;
+		return access.get().get() && isAnyNeighborBackground(dimension, access, position);
 	}
 	// endregion
 }
