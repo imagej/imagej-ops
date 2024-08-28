07c65b9f6c4f2202e29c9bd50bc7c69a78e5f2cf
tpietzsch <tobias.pietzsch@gmail.com>
2024-08-28 13:30:42 +0200

    Fix remaining compile error (related to imglib/imglib2-roi#71)

--- src/main/java/net/imagej/ops/labeling/MergeLabeling.java
+++ src/main/java/net/imagej/ops/labeling/MergeLabeling.java
@@ -92,7 +92,7 @@ public class MergeLabeling<L, I extends IntegerType<I>, B extends BooleanType<B>
 	{
 		if (mask != null) {
 			final IterableRegion iterable = Regions.iterable(mask);
-			final IterableInterval<LabelingType<L>> sample = Regions.sample(iterable,
+			final IterableInterval<LabelingType<L>> sample = Regions.sample(iterable.inside(),
 				output);
 			final RandomAccess<LabelingType<L>> randomAccess = input1.randomAccess();
 			final RandomAccess<LabelingType<L>> randomAccess2 = input2.randomAccess();
